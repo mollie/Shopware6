4d16028ce93155ec9643a16bf165a4293726053f
Daniel Schipper <daniel@memo-ict.nl>
2021-01-14 10:50:43 +0100

    Up version

--- src/Factory/MollieApiFactory.php
+++ src/Factory/MollieApiFactory.php
@@ -86,7 +86,7 @@ class MollieApiFactory
 
             // @todo Add plugin version variable
             $this->apiClient->addVersionString(
-                'MollieShopware6/1.3.9'
+                'MollieShopware6/1.3.10'
             );
         } catch (Exception $e) {
             $this->logger->error($e->getMessage(), [$e]);
