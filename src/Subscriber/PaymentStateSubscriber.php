103d3c0d992ae5c45de7b394c43675ed9ae099c1
carsten <carsten.schmitz@dasistweb.de>
2021-04-16 12:50:48 +0200

    MOL-384: refactor PaymentStateSubscriber

--- src/Subscriber/PaymentStateSubscriber.php
+++ src/Subscriber/PaymentStateSubscriber.php
@@ -2,120 +2,70 @@
 
 namespace Kiener\MolliePayments\Subscriber;
 
-use Kiener\MolliePayments\Service\TransactionService;
-use Mollie\Api\Exceptions\ApiException;
-use Mollie\Api\MollieApiClient;
-use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionEntity;
-use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionStates;
-use Shopware\Core\Checkout\Order\OrderEvents;
-use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenEvent;
-use Shopware\Core\Framework\DataAbstractionLayer\Exception\InconsistentCriteriaIdsException;
+use Kiener\MolliePayments\Exception\CouldNotSetRefundAtMollieException;
+use Kiener\MolliePayments\Facade\SetMollieOrderRefunded;
+use Kiener\MolliePayments\Service\LoggerService;
+use Monolog\Logger;
+use Shopware\Core\System\StateMachine\Aggregation\StateMachineTransition\StateMachineTransitionActions;
+use Shopware\Core\System\StateMachine\Event\StateMachineStateChangeEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class PaymentStateSubscriber implements EventSubscriberInterface
 {
-    /** @var MollieApiClient $apiClient */
-    private $apiClient;
-
-    /** @var TransactionService */
-    private $transactionService;
-
     /**
-     * Returns an array of event names this subscriber wants to listen to.
-     *
-     * The array keys are event names and the value can be:
-     *
-     * * The method name to call (priority defaults to 0)
-     * * An array composed of the method name to call and the priority
-     * * An array of arrays composed of the method names to call and respective
-     *   priorities, or 0 if unset
-     *
-     * For instance:
-     *
-     * * array('eventName' => 'methodName')
-     * * array('eventName' => array('methodName', $priority))
-     * * array('eventName' => array(array('methodName1', $priority), array('methodName2'))
-     *
-     * @return array The event names to listen to
+     * @var LoggerService
      */
-    public static function getSubscribedEvents()
-    {
-        return [
-            OrderEvents::ORDER_TRANSACTION_WRITTEN_EVENT => 'onOrderTransactionWritten'
-        ];
-    }
+    private $loggerService;
+    /**
+     * @var SetMollieOrderRefunded
+     */
+    private $setMollieOrderRefunded;
 
     /**
      * Creates a new instance of PaymentMethodSubscriber.
      *
-     * @param MollieApiClient $apiClient
-     * @param TransactionService $transactionService
+     * @param SetMollieOrderRefunded $setMollieOrderRefunded
+     * @param LoggerService $loggerService
      */
     public function __construct(
-        MollieApiClient $apiClient,
-        TransactionService $transactionService
+        SetMollieOrderRefunded $setMollieOrderRefunded,
+        LoggerService $loggerService
     )
     {
-        $this->apiClient = $apiClient;
-        $this->transactionService = $transactionService;
+        $this->setMollieOrderRefunded = $setMollieOrderRefunded;
+        $this->loggerService = $loggerService;
     }
 
-    /**
-     * Refunds the transaction at Mollie if the payment state is refunded.
-     *
-     * @param EntityWrittenEvent $args
-     * @throws ApiException
-     */
-    public function onOrderTransactionWritten(EntityWrittenEvent $args): void
+    public static function getSubscribedEvents()
     {
-        foreach ($args->getPayloads() as $payload) {
-            $transactionId = $payload['id'];
-            $transactionVersionId = $payload['versionId'];
-            $order = null;
-            $customFields = null;
-            $mollieOrder = null;
+        return [
+            'state_machine.order_transaction.state_changed' => 'onOrderTransactionChanged',
+        ];
+    }
 
-            try {
-                /** @var OrderTransactionEntity $transaction */
-                $transaction = $this->transactionService->getTransactionById(
-                    $transactionId,
-                    $transactionVersionId
-                );
-            } catch (InconsistentCriteriaIdsException $e) {
-                // @todo Handle exception
-            }
 
-            // Get the order from the transaction
-            if (
-                $transaction !== null
-                && $transaction->getStateMachineState() !== null
-                && $transaction->getStateMachineState()->getTechnicalName() === 'refunded'
-            ) {
-                $order = $transaction->getOrder();
-            }
+    public function onOrderTransactionChanged(StateMachineStateChangeEvent $event)
+    {
+        if ($event->getTransitionSide() !== StateMachineStateChangeEvent::STATE_MACHINE_TRANSITION_SIDE_ENTER) {
+            return;
+        }
 
-            // Get the custom fields from the order
-            if (
-                $order !== null
-                && $order->getCustomFields() !== null
-            ) {
-                $customFields = $order->getCustomFields();
-            }
+        $transitionName = $event->getTransition()->getTransitionName();
 
-            // Get the order at Mollie
-            if (
-                $customFields !== null
-                && isset($customFields['mollie_payments']['order_id'])
-            ) {
-                $mollieOrder = $this->apiClient->orders->get(
-                    $customFields['mollie_payments']['order_id']
-                );
-            }
+        if ($transitionName != StateMachineTransitionActions::ACTION_REFUND) {
+            return;
+        }
 
-            // Refund the order
-            if ($mollieOrder !== null) {
-                $mollieOrder->refundAll();
-            }
+        try {
+            $this->setMollieOrderRefunded->setRefunded($event->getTransition()->getEntityId(), $event->getContext());
+        } catch (CouldNotSetRefundAtMollieException $e) {
+            $this->loggerService->addEntry(
+                $e->getMessage(),
+                $event->getContext(),
+                $e,
+                [],
+                Logger::ERROR
+            );
         }
     }
 }
