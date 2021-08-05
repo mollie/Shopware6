ecd06ff49f1c52786f1b0c47acd903b09b735aed
Carsten Schmitz <64227129+schmitzcarsten@users.noreply.github.com>
2021-08-05 17:01:57 +0200

    prepare release 1.5.2 (#175)

--- src/Factory/MollieApiFactory.php
+++ src/Factory/MollieApiFactory.php
@@ -83,7 +83,7 @@ class MollieApiFactory
 
             // @todo Add plugin version variable
             $this->apiClient->addVersionString(
-                'MollieShopware6/1.5.1'
+                'MollieShopware6/1.5.2'
             );
         } catch (Exception $e) {
             $this->logger->error($e->getMessage(), [$e]);
