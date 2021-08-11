6f94a458c1d08c38daa0a11a162fded802c86469
carsten <carsten.schmitz@dasistweb.de>
2021-08-11 10:06:19 +0200

    add di at runtime

--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -2,6 +2,7 @@
 
 namespace Kiener\MolliePayments\Service;
 
+use Psr\Container\ContainerInterface;
 use Shopware\Core\Checkout\Cart\Cart;
 use Shopware\Core\Checkout\Cart\LineItem\LineItem;
 use Shopware\Core\Checkout\Cart\SalesChannel\CartService as SalesChannelCartService;
@@ -10,7 +11,6 @@ use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Payment\PaymentMethodEntity;
 use Shopware\Core\Checkout\Shipping\ShippingMethodEntity;
 use Shopware\Core\Framework\Uuid\Uuid;
-use Shopware\Core\System\SalesChannel\Context\SalesChannelContextFactory;
 use Shopware\Core\System\SalesChannel\Context\SalesChannelContextService;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 
@@ -19,21 +19,18 @@ class CartService
     /** @var SalesChannelCartService */
     private $salesChannelCartService;
 
-    /** @var SalesChannelContextFactory */
-    private $salesChannelContextFactory;
-
     /**
-     * Creates a new instance of the cart service.
-     *
-     * @param SalesChannelCartService $salesChannelCartService
+     * @var ContainerInterface
      */
+    private ContainerInterface $container;
+
     public function __construct(
         SalesChannelCartService $salesChannelCartService,
-        SalesChannelContextFactory $salesChannelContextFactory
+        ContainerInterface $container
     )
     {
         $this->salesChannelCartService = $salesChannelCartService;
-        $this->salesChannelContextFactory = $salesChannelContextFactory;
+        $this->container = $container;
     }
 
     /**
@@ -122,7 +119,12 @@ class CartService
             $options[SalesChannelContextService::SHIPPING_ADDRESS_ID] = $shippingAddressId;
         }
 
-        $newSalesChannelContext = $this->salesChannelContextFactory->create(
+        // because of backwards compatibility we have to get the factory on runtime via container. With 6.4 we would have to inject
+        // the abstract version as dependency (the cached version could be injected).
+        // The abstractFactory does not exist for sw versions < 6.4
+        $salesChannelContextFactory = $this->container->get("Shopware\Core\System\SalesChannel\Context\SalesChannelContextFactory");
+
+        $newSalesChannelContext = $salesChannelContextFactory->create(
             $salesChannelContext->getToken(),
             $salesChannelContext->getSalesChannel()->getId(),
             $options
