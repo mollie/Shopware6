98f3e159c901798ff5fc9e0de66f720c4d88f201
boxblinkracer <boxblinkracer@gmx.net>
2021-10-26 12:28:05 +0200

    NTR: new release 1.5.5

--- src/MolliePayments.php
+++ src/MolliePayments.php
@@ -21,7 +21,7 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 class MolliePayments extends Plugin
 {
 
-    const PLUGIN_VERSION = '1.5.4';
+    const PLUGIN_VERSION = '1.5.5';
 
     /**
      * @param ContainerBuilder $container
