6e0d8f21cc78ab847646ab7f84e053373ded0431
Daniel Schipper <dev@kiener.nl>
2021-11-11 14:48:44 +0100

    Entities always have getTranslated, so make sure customFields exist

--- src/Struct/Attribute/EntityAttributeStruct.php
+++ src/Struct/Attribute/EntityAttributeStruct.php
@@ -23,16 +23,13 @@ abstract class EntityAttributeStruct extends AttributeStruct
             throw new \Exception('Entity does not contain custom fields');
         }
 
-        /** TODO 003: Entities always have getTranslated.
-         * If this entity is translatable, for example, product or category, then a method getTranslated is available.
+        /**
          * Use the custom fields from this translated array instead of the regular ones.
          *
          * The getTranslated array contains all the translated fields of this entity, merged on top of the data
          * in the same fields from the default system language.
          */
-        $customFields = method_exists($entity, 'getTranslated')
-            ? $entity->getTranslated()['customFields']
-            : $entity->getCustomFields();
+        $customFields = $entity->getTranslation('customFields') ?? $entity->getCustomFields();
 
         /**
          * If we don't have any custom fields, stop and return.
