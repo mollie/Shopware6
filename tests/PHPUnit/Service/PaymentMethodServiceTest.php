e4da42947c877dd9e1cdd102f88605881f48fb3a
Reinder van Bochove <work@iamreinder.dev>
2021-12-08 13:00:44 +0100

    MOL-629 Renamed a test method for consistency.

--- tests/PHPUnit/Service/PaymentMethodServiceTest.php
+++ tests/PHPUnit/Service/PaymentMethodServiceTest.php
@@ -235,7 +235,7 @@ class PaymentMethodServiceTest extends TestCase
         );
     }
 
-    public function testDoesOnlyActivateNewlyInstalledPaymentMethod(): void
+    public function testDoesOnlyActivateNewlyInstalledPaymentMethods(): void
     {
         $paymentMethodIds = ['112233', '445566'];
 
