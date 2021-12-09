2ae14fd431c1a6567f24d93ef8392a38bad00bd0
Daniel Schipper <dev@kiener.nl>
2021-12-09 15:31:48 +0100

    Test if we get correct state/action for corresponding Shopware version

---
+++ tests/PHPUnit/Compatibility/CompatibilityGatewayOrderTransactionStateTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace MolliePayments\Tests\Compatibility;
+
+use Kiener\MolliePayments\Compatibility\Gateway\CompatibilityGateway;
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\System\SalesChannel\Context\SalesChannelContextService;
+
+class CompatibilityGatewayOrderTransactionStateTest extends TestCase
+{
+    private $salesChannelContextService;
+
+
+    protected function setUp(): void
+    {
+        $this->salesChannelContextService = $this->createMock(SalesChannelContextService::class);
+    }
+
+    /**
+     * @param $swVersion
+     * @param $expectedState
+     * @dataProvider getChargebackStateTestData
+     */
+    public function testGetChargebackState($swVersion, $expectedState) {
+        $compatibilityGateway = new CompatibilityGateway($swVersion, $this->salesChannelContextService);
+
+        $actualState = $compatibilityGateway->getChargebackOrderTransactionState();
+
+        $this->assertEquals($expectedState, $actualState);
+    }
+
+    /**
+     * @param $swVersion
+     * @param $expectedState
+     * @dataProvider getChargebackActionTestData
+     */
+    public function testGetChargebackAction($swVersion, $expectedState) {
+        $compatibilityGateway = new CompatibilityGateway($swVersion, $this->salesChannelContextService);
+
+        $actualState = $compatibilityGateway->getChargebackOrderTransactionAction();
+
+        $this->assertEquals($expectedState, $actualState);
+    }
+
+    public function getChargebackStateTestData()
+    {
+        return [
+            'Chargeback state in Shopware 6.2.3 and higher' => ['6.2.3', 'chargeback'],
+            'Chargeback state in Shopware 6.2.2 and lower' => ['6.2.2', 'in_progress'],
+        ];
+    }
+
+    public function getChargebackActionTestData()
+    {
+        return [
+            'Chargeback action in Shopware 6.2.3 and higher' => ['6.2.3', 'chargeback'],
+            'Chargeback action in Shopware 6.2.2 and lower' => ['6.2.2', 'do_pay'],
+        ];
+    }
+}
