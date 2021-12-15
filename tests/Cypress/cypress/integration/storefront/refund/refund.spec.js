fd76eecc0768e810cb7fe232cd31f98ce5510f4c
boxblinkracer <boxblinkracer@gmx.net>
2021-12-15 11:45:46 +0100

    NTR: improve cypress refund stability again

--- tests/Cypress/cypress/integration/storefront/refund/refund.spec.js
+++ tests/Cypress/cypress/integration/storefront/refund/refund.spec.js
@@ -87,7 +87,7 @@ context("Order Refunds", () => {
             // after refunded, open the refund manager
             // and verify that we see a PENDING refund in it as well as
             // the correct 1 EUR value.
-            cy.wait(500);
+            cy.wait(1000);
             repoOrdersDetails.getMollieRefundManagerButton().click();
             repoRefundManager.getFirstRefundStatusLabel().contains('Pending');
 
@@ -124,7 +124,7 @@ context("Order Refunds", () => {
 
             // let's open the refund manager, and verify
             // that the pending refund is NOT existing anymore
-            cy.wait(500);
+            cy.wait(1000);
             repoOrdersDetails.getMollieRefundManagerButton().click();
             cy.contains('Pending').should('not.exist');
         })
