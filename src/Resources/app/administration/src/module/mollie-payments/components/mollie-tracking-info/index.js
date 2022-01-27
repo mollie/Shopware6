3d9a39a394f090d532cb59707a12a14f20153515
Kiener Digital Commerce <39724314+KienerNL@users.noreply.github.com>
2022-01-27 16:05:09 +0100

    MOL-719: Show existing trackingcodes and prefill tracking info (#277)

--- src/Resources/app/administration/src/module/mollie-payments/components/mollie-tracking-info/index.js
+++ src/Resources/app/administration/src/module/mollie-payments/components/mollie-tracking-info/index.js
@@ -1,12 +1,23 @@
 import template from './mollie-tracking-info.html.twig';
+import './mollie-tracking-info.scss';
 
 // eslint-disable-next-line no-undef
 const {Component} = Shopware;
+// eslint-disable-next-line no-undef
+const {string} = Shopware.Utils;
 
 Component.register('mollie-tracking-info', {
     template,
 
     props: {
+        delivery: {
+            type: Object,
+            required: true,
+            default() {
+                return null
+            },
+        },
+
         tracking: {
             type: Object,
             required: true,
@@ -17,6 +28,36 @@ Component.register('mollie-tracking-info', {
                     url: '',
                 }
             },
+        },
+    },
+
+    created() {
+        this.createdComponent();
+    },
+
+    methods: {
+        createdComponent() {
+            if(this.delivery.trackingCodes.length === 1) {
+                this.prefillTrackingInfo(this.delivery.trackingCodes[0], this.delivery.shippingMethod);
+            }
+        },
+
+        prefillTrackingInfo(trackingCode, shippingMethod) {
+            this.tracking.carrier = shippingMethod.name;
+            this.tracking.code = trackingCode;
+
+            if(!string.isEmptyOrSpaces(shippingMethod.trackingUrl)) {
+                this.tracking.url = this.renderTrackingUrl(trackingCode, shippingMethod);
+            }
+        },
+
+        /**
+         * Copied from src/Administration/Resources/app/administration/src/module/sw-order/component/sw-order-user-card/index.js
+         */
+        renderTrackingUrl(trackingCode, shippingMethod) {
+            const urlTemplate = shippingMethod ? shippingMethod.trackingUrl : null;
+
+            return urlTemplate ? urlTemplate.replace('%s', encodeURIComponent(trackingCode)) : '';
         },
     },
 });
