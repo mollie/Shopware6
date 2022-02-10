c720b8caf65de3ea32ac45697603d9c68b2b3676
Daniel Schipper <dev@kiener.nl>
2022-02-10 15:39:46 +0100

    Update HttpClient to allow POST, send data, and auto parse json response

--- src/Resources/app/storefront/src/mollie-payments/services/HttpClient.js
+++ src/Resources/app/storefront/src/mollie-payments/services/HttpClient.js
@@ -1,36 +1,59 @@
 const DEFAULT_CONTENT_TYPE = 'application/json';
 
 export default class HttpClient {
-
-    /**
-     * Constructor.
-     */
-    constructor() {
-        this.request = null;
-    }
-
     /**
      * Request GET
      * @param {string} url
      * @param {function} callbackSuccess
      * @param {function} callbackError
      * @param {string} contentType
-     * @returns {XMLHttpRequest}
      */
     get(url, callbackSuccess, callbackError, contentType = DEFAULT_CONTENT_TYPE) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.setRequestHeader('Content-Type', contentType);
-
-        xhr.onload = function (response) {
-            callbackSuccess(response);
-        };
-
-        xhr.onerror = function (response) {
-            callbackError(response);
-        };
-
-        xhr.send();
+        this.send('GET', url, null, callbackSuccess, callbackError, contentType);
     }
 
+    /**
+     * Request POST
+     * @param {string} url
+     * @param {*} data
+     * @param {function} callbackSuccess
+     * @param {function} callbackError
+     * @param {string} contentType
+     */
+    post(url, data, callbackSuccess, callbackError, contentType = DEFAULT_CONTENT_TYPE) {
+        this.send('POST', url, data, callbackSuccess, callbackError, contentType);
+    }
+
+    /**
+     * Sends an XMLHttpRequest
+     * @param {string} type
+     * @param {string} url
+     * @param {*} data
+     * @param {function} callbackSuccess
+     * @param {function} callbackError
+     * @param {string} contentType
+     */
+    send(type, url, data, callbackSuccess, callbackError, contentType = DEFAULT_CONTENT_TYPE)
+    {
+        const xhr = new XMLHttpRequest();
+        xhr.open(type, url);
+        xhr.setRequestHeader('Content-Type', contentType);
+
+        xhr.onload = function () {
+            const responseType = xhr.getResponseHeader('content-type');
+            const body = 'response' in xhr ? xhr.response : xhr.responseText
+
+            if(responseType.indexOf('application/json') > -1) {
+                callbackSuccess(JSON.parse(body));
+            } else {
+                callbackSuccess(body);
+            }
+        };
+
+        xhr.onerror = function () {
+            callbackError();
+        };
+
+        xhr.send(data);
+    }
 }
