825fa073ef59b8e930150fdd70d499649ddab9a3
Daniel Schipper <daniel@memo-ict.nl>
2022-03-10 16:37:33 +0100

    Move functions to abstract class

--- src/Service/Mail/AbstractMailService.php
+++ src/Service/Mail/AbstractMailService.php
@@ -2,8 +2,10 @@
 
 namespace Kiener\MolliePayments\Service\Mail;
 
-use Shopware\Core\Content\MailTemplate\Exception\MailTransportFailedException;
+use Shopware\Core\Framework\Validation\DataValidationDefinition;
 use Shopware\Core\Framework\Validation\Exception\ConstraintViolationException;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 abstract class AbstractMailService
 {
@@ -15,9 +17,37 @@ abstract class AbstractMailService
     /**
      * @param array<string, mixed> $data
      * @param array<array<string, mixed>> $attachments
-     * @throws ConstraintViolationException
-     * @throws MailTransportFailedException
      * @return void
+     * @throws ConstraintViolationException
      */
-    public function send(array $data, array $attachments = []): void;
+    public abstract function send(array $data, array $attachments = []): void;
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    protected function buildContents(array $data): array
+    {
+        $plain = strip_tags(str_replace(['<div>', '</div>'], "\r\n", $data['contentHtml']));
+
+        return [
+            'text/html' => '<div style="font-family:arial; font-size:12px;">' . $data['contentHtml'] . '</div>',
+            'text/plain' => $plain,
+        ];
+    }
+
+    /**
+     * @return DataValidationDefinition
+     */
+    protected function getValidationDefinition(): DataValidationDefinition
+    {
+        $definition = new DataValidationDefinition('mail_service.send');
+
+        $definition->add('contentHtml', new NotBlank());
+        $definition->add('subject', new NotBlank());
+        $definition->add('senderName', new NotBlank());
+        $definition->add('senderEmail', new NotBlank(), new Email());
+
+        return $definition;
+    }
 }
--- src/Service/Mail/MailService.php
+++ src/Service/Mail/MailService.php
@@ -2,16 +2,11 @@
 
 namespace Kiener\MolliePayments\Service\Mail;
 
-use Kiener\MolliePayments\Exception\MailBodyEmptyException;
 use Psr\Log\LoggerInterface;
 use Shopware\Core\Content\Mail\Service\AbstractMailFactory;
 use Shopware\Core\Content\Mail\Service\AbstractMailSender;
 use Shopware\Core\Content\MailTemplate\Exception\MailTransportFailedException;
-use Shopware\Core\Framework\Validation\DataValidationDefinition;
 use Shopware\Core\Framework\Validation\DataValidator;
-use Shopware\Core\Framework\Validation\Exception\ConstraintViolationException;
-use Symfony\Component\Validator\Constraints\Email as EmailConstraint;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class MailService extends AbstractMailService
 {
@@ -49,11 +44,8 @@ class MailService extends AbstractMailService
     }
 
     /**
-     * @param array $data
-     * @param array $attachments
-     * @throws ConstraintViolationException
+     * @inheritDoc
      * @throws MailTransportFailedException
-     * @return void
      */
     public function send(array $data, array $attachments = []): void
     {
@@ -87,28 +79,4 @@ class MailService extends AbstractMailService
 
         $this->mailSender->send($mail);
     }
-
-    private function buildContents(array $data): array
-    {
-        $plain = strip_tags(str_replace(['<div>', '</div>'], "\r\n", $data['contentHtml']));
-
-        return [
-            'text/html' => '<div style="font-family:arial; font-size:12px;">' . $data['contentHtml'] . '</div>',
-            'text/plain' => $plain,
-        ];
-    }
-
-    private function getValidationDefinition(): DataValidationDefinition
-    {
-        $definition = new DataValidationDefinition('mail_service.send');
-
-        $definition->add('contentHtml', new NotBlank());
-        $definition->add('subject', new NotBlank());
-        $definition->add('senderName', new NotBlank());
-        $definition->add('senderEmail', new NotBlank(), new EmailConstraint());
-
-        return $definition;
-    }
-
-
 }
