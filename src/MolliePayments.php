dd03377266276aaa79323bc95e9b743079e8c554
Christian Dangl <boxblinkracer@gmx.net>
2022-05-11 10:36:05 +0200

    Release 2.2.2

--- src/MolliePayments.php
+++ src/MolliePayments.php
@@ -19,7 +19,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 class MolliePayments extends Plugin
 {
 
-    const PLUGIN_VERSION = '2.2.1';
+    const PLUGIN_VERSION = '2.2.2';
 
     /**
      * @param ContainerBuilder $container
