046fa0294335cafbfe65565920f5323e50118be7
Christian Dangl <boxblinkracer@gmx.net>
2022-05-18 16:54:57 +0200

    NTR: update custom address form

--- src/Storefront/Controller/AccountController.php
+++ src/Storefront/Controller/AccountController.php
@@ -13,7 +13,6 @@ use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
