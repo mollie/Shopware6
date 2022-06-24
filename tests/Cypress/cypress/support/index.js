656d153f185823ef22a3080a79cfb1ba24c3f9f2
Christian Dangl <boxblinkracer@gmx.net>
2022-06-24 09:23:17 +0200

    NTR: add cypress-testrail npm package

--- tests/Cypress/cypress/support/index.js
+++ tests/Cypress/cypress/support/index.js
@@ -16,7 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import axios from "axios";
-
+import 'cypress-testrail';
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
@@ -24,80 +24,3 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     // cause an error in the console which stops the test
     return false
 })
-
-Cypress.on('test:after:run', (data) => {
-
-    const caseId = extractCaseId(data.title);
-
-    if (caseId === '') {
-        return;
-    }
-
-    let status = 1;
-
-    if (data.state === 'passed') {
-        status = 1;
-    } else {
-        status = 5;
-    }
-
-    const config = Cypress.env('testrail');
-
-    if (config.domain === null || config.domain === '') {
-        return;
-    }
-
-    sendResult(
-        config.runId,
-        caseId,
-        status,
-        config.domain,
-        config.username,
-        config.password
-    )
-    ;
-})
-
-function extractCaseId(title) {
-    if (title.includes(':', 0)) {
-
-        if (title.startsWith('C')) {
-
-            const caseId = title.substring(1, title.indexOf(":"));
-            return caseId;
-        }
-    }
-
-    return '';
-}
-
-function sendResult(runID, caseId, status, domain, username, password) {
-
-    const postData = {
-        "results": [
-            {
-                "case_id": caseId,
-                "status_id": status,
-                "comment": 'Tested by Cypress'
-            }
-        ]
-    };
-
-    axios(
-        {
-            method: 'post',
-            url: `https://${domain}/index.php?/api/v2/add_results_for_cases/${runID}`,
-            headers: {'Content-Type': 'application/json'},
-            auth: {
-                username: username,
-                password: password,
-            },
-            data: JSON.stringify(postData),
-        })
-        .then(response => {
-            console.log("Sent TestRail result for TestCase " + caseId);
-        })
-        .catch(error => {
-            console.error(error)
-        });
-}
