03a789fe4c660407211b93c10bb48e06aeebf49a
boxblinkracer <boxblinkracer@gmx.net>
2022-06-28 16:57:59 +0200

    MOL-675: Add Cypress tests for reference numbers in Admin

--- tests/Cypress/cypress/integration/storefront/payment-methods/banktransfer.js
+++ tests/Cypress/cypress/integration/storefront/payment-methods/banktransfer.js
@@ -8,15 +8,22 @@ import PaymentAction from "Actions/storefront/checkout/PaymentAction";
 import DummyBasketScenario from "Scenarios/DummyBasketScenario";
 import CheckoutAction from "Actions/storefront/checkout/CheckoutAction";
 import PaymentScreenAction from "Actions/mollie/PaymentScreenAction";
+import OrderDetailsRepository from "Repositories/admin/orders/OrderDetailsRepository";
+import AdminOrdersAction from "Actions/admin/AdminOrdersAction";
+import AdminLoginAction from "Actions/admin/AdminLoginAction";
 
 
 const devices = new Devices();
 const session = new Session();
 const shopware = new Shopware();
 
+const repoOrdersDetails = new OrderDetailsRepository();
+
 const configAction = new ShopConfigurationAction();
 const paymentAction = new PaymentAction();
 const checkout = new CheckoutAction();
+const adminOrders = new AdminOrdersAction();
+const adminLogin = new AdminLoginAction();
 
 const molliePayment = new PaymentScreenAction();
 
@@ -51,6 +58,27 @@ describe('SEPA Bank Transfer', () => {
 
                 cy.url().should('include', '/checkout/finish');
                 cy.contains('Thank you for your order');
+            })
+
+            it('C6961: Banktransfer Reference Number is visible in Administration', () => {
+
+                scenarioDummyBasket.execute();
+
+                paymentAction.switchPaymentMethod('Banktransfer');
+
+                shopware.prepareDomainChange();
+                checkout.placeOrderOnConfirm();
+
+                molliePayment.initSandboxCookie();
+                molliePayment.selectPaid();
+
+                adminLogin.login();
+
+                adminOrders.openOrders();
+                adminOrders.openLastOrder();
+
+                repoOrdersDetails.getPaymentReferenceTitle().should('exist');
+                repoOrdersDetails.getPaymentReferenceValue().should('exist');
             })
 
         })
--- tests/Cypress/cypress/integration/storefront/payment-methods/paypal.js
+++ tests/Cypress/cypress/integration/storefront/payment-methods/paypal.js
@@ -8,15 +8,22 @@ import PaymentAction from "Actions/storefront/checkout/PaymentAction";
 import DummyBasketScenario from "Scenarios/DummyBasketScenario";
 import CheckoutAction from "Actions/storefront/checkout/CheckoutAction";
 import PaymentScreenAction from "Actions/mollie/PaymentScreenAction";
+import AdminOrdersAction from "Actions/admin/AdminOrdersAction";
+import AdminLoginAction from "Actions/admin/AdminLoginAction";
+import OrderDetailsRepository from "Repositories/admin/orders/OrderDetailsRepository";
 
 
 const devices = new Devices();
 const session = new Session();
 const shopware = new Shopware();
 
+const repoOrdersDetails = new OrderDetailsRepository();
+
 const configAction = new ShopConfigurationAction();
 const paymentAction = new PaymentAction();
 const checkout = new CheckoutAction();
+const adminOrders = new AdminOrdersAction();
+const adminLogin = new AdminLoginAction();
 
 const molliePayment = new PaymentScreenAction();
 
@@ -51,6 +58,27 @@ describe('PayPal', () => {
 
                 cy.url().should('include', '/checkout/finish');
                 cy.contains('Thank you for your order');
+            })
+
+            it('C6960: Paypal Reference Number is visible in Administration', () => {
+
+                scenarioDummyBasket.execute();
+
+                paymentAction.switchPaymentMethod('PayPal');
+
+                shopware.prepareDomainChange();
+                checkout.placeOrderOnConfirm();
+
+                molliePayment.initSandboxCookie();
+                molliePayment.selectPaid();
+
+                adminLogin.login();
+
+                adminOrders.openOrders();
+                adminOrders.openLastOrder();
+
+                repoOrdersDetails.getPaymentReferenceTitle().should('exist');
+                repoOrdersDetails.getPaymentReferenceValue().should('exist');
             })
 
         })
