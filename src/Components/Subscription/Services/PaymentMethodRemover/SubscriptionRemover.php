fffd64dd96e6a63855e236667d2a269ced2ddfdd
Daniel Schipper <daniel@memo-ict.nl>
2022-06-30 16:20:36 +0200

    Update subscription remover for order edit page

--- src/Components/Subscription/Services/PaymentMethodRemover/SubscriptionRemover.php
+++ src/Components/Subscription/Services/PaymentMethodRemover/SubscriptionRemover.php
@@ -2,12 +2,22 @@
 
 namespace Kiener\MolliePayments\Components\Subscription\Services\PaymentMethodRemover;
 
+use Kiener\MolliePayments\Service\MollieApi\OrderDataExtractor;
+use Kiener\MolliePayments\Service\OrderService;
 use Kiener\MolliePayments\Service\Payment\Remover\PaymentMethodRemover;
+use Kiener\MolliePayments\Service\SettingsService;
 use Kiener\MolliePayments\Struct\LineItem\LineItemAttributes;
+use Kiener\MolliePayments\Struct\OrderLineItemEntity\OrderLineItemEntityAttributes;
 use Kiener\MolliePayments\Struct\PaymentMethod\PaymentMethodAttributes;
+use Psr\Log\LoggerInterface;
 use Shopware\Core\Checkout\Cart\Cart;
+use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemEntity;
+use Shopware\Core\Checkout\Order\OrderEntity;
 use Shopware\Core\Checkout\Payment\SalesChannel\PaymentMethodRouteResponse;
+use Shopware\Core\Framework\Context;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class SubscriptionRemover extends PaymentMethodRemover
 {
@@ -24,6 +34,26 @@ class SubscriptionRemover extends PaymentMethodRemover
     ];
 
     /**
+     * @var OrderDataExtractor
+     */
+    private $orderDataExtractor;
+
+    /**
+     * @param ContainerInterface $container
+     * @param RequestStack       $requestStack
+     * @param OrderService       $orderService
+     * @param SettingsService    $settingsService
+     * @param OrderDataExtractor $orderDataExtractor
+     * @param LoggerInterface    $logger
+     */
+    public function __construct(ContainerInterface $container, RequestStack $requestStack, OrderService $orderService, SettingsService $settingsService, OrderDataExtractor $orderDataExtractor, LoggerInterface $logger)
+    {
+        parent::__construct($container, $requestStack, $orderService, $settingsService, $logger);
+
+        $this->orderDataExtractor = $orderDataExtractor;
+    }
+
+    /**
      * @param PaymentMethodRouteResponse $originalData
      * @param SalesChannelContext        $context
      * @return PaymentMethodRouteResponse
@@ -31,9 +61,21 @@ class SubscriptionRemover extends PaymentMethodRemover
      */
     public function removePaymentMethods(PaymentMethodRouteResponse $originalData, SalesChannelContext $context): PaymentMethodRouteResponse
     {
-        $cart = $this->getCartServiceLazy()->getCart($context->getToken(), $context);
+        if ($this->isCartRoute()) {
+            $cart = $this->getCart($context);
+            $isSubscription = $this->isSubscriptionCart($cart);
+        }
 
-        if (!$this->isSubscriptionCart($cart)) {
+        if ($this->isOrderRoute()) {
+            $order = $this->getOrder($context->getContext());
+            $isSubscription = $this->isSubscriptionOrder($order, $context->getContext());
+        }
+
+        if(!isset($isSubscription)) {
+            $isSubscription = false;
+        }
+
+        if(!$isSubscription) {
             return $originalData;
         }
 
@@ -62,6 +104,27 @@ class SubscriptionRemover extends PaymentMethodRemover
             $attribute = new LineItemAttributes($lineItem);
 
             if ($attribute->isSubscriptionProduct()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param OrderEntity $order
+     * @param Context     $context
+     * @return bool
+     */
+    private function isSubscriptionOrder(OrderEntity $order, Context $context): bool
+    {
+        $lineItems = $this->orderDataExtractor->extractLineItems($order, $context);
+
+        /** @var OrderLineItemEntity $lineItem */
+        foreach ($lineItems as $lineItem) {
+            $attributes = new OrderLineItemEntityAttributes($lineItem);
+
+            if($attributes->isSubscriptionProduct()) {
                 return true;
             }
         }
