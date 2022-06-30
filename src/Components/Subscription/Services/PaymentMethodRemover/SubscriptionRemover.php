088e8ef6d0b307bb3ddffb2bb2a5161b429651e9
Daniel Schipper <daniel@memo-ict.nl>
2022-06-30 13:28:50 +0200

    Refactor subscription remover

--- src/Components/Subscription/Services/PaymentMethodRemover/SubscriptionRemover.php
+++ src/Components/Subscription/Services/PaymentMethodRemover/SubscriptionRemover.php
@@ -2,20 +2,14 @@
 
 namespace Kiener\MolliePayments\Components\Subscription\Services\PaymentMethodRemover;
 
-
-use Kiener\MolliePayments\Service\OrderService;
-use Kiener\MolliePayments\Service\SettingsService;
+use Kiener\MolliePayments\Service\Payment\Remover\PaymentMethodRemover;
 use Kiener\MolliePayments\Struct\LineItem\LineItemAttributes;
 use Kiener\MolliePayments\Struct\PaymentMethod\PaymentMethodAttributes;
-use Psr\Log\LoggerInterface;
 use Shopware\Core\Checkout\Cart\Cart;
-use Shopware\Core\Checkout\Cart\SalesChannel\CartService;
 use Shopware\Core\Checkout\Payment\SalesChannel\PaymentMethodRouteResponse;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 
-class SubscriptionRemover
+class SubscriptionRemover extends PaymentMethodRemover
 {
 
     public const ALLOWED_METHODS = [
@@ -29,52 +23,9 @@ class SubscriptionRemover
         'paypal',
     ];
 
-
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
-
-    /**
-     * @var SettingsService
-     */
-    private $settingsService;
-
-    /**
-     * @var OrderService
-     */
-    private $orderService;
-
-    /**
-     * @var RequestStack
-     */
-    private $requestStack;
-
-    /**
-     * @var LoggerInterface
-     */
-    private $logger;
-
-    /**
-     * @param ContainerInterface $container
-     * @param SettingsService    $settingsService
-     * @param OrderService       $orderService
-     * @param RequestStack       $requestStack
-     * @param LoggerInterface    $logger
-     */
-    public function __construct(ContainerInterface $container, SettingsService $settingsService, OrderService $orderService, RequestStack $requestStack, LoggerInterface $logger)
-    {
-        $this->container = $container;
-        $this->settingsService = $settingsService;
-        $this->orderService = $orderService;
-        $this->requestStack = $requestStack;
-        $this->logger = $logger;
-    }
-
-
     /**
      * @param PaymentMethodRouteResponse $originalData
-     * @param SalesChannelContext $context
+     * @param SalesChannelContext        $context
      * @return PaymentMethodRouteResponse
      * @throws \Exception
      */
@@ -117,23 +68,4 @@ class SubscriptionRemover
 
         return false;
     }
-
-    /**
-     * We have to use lazy loading for this. Otherwise there are plugin compatibilities
-     * with a circular reference...even though XML looks fine.
-     *
-     * @return CartService
-     * @throws \Exception
-     */
-    private function getCartServiceLazy(): CartService
-    {
-        $service = $this->container->get('Shopware\Core\Checkout\Cart\SalesChannel\CartService');
-
-        if (!$service instanceof CartService) {
-            throw new \Exception('CartService of Shopware not found!');
-        }
-
-        return $service;
-    }
-
 }
