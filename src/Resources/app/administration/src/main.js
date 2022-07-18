6da5d607368757e8e1b4caacc18e7cfd7215212e
Spencer Shaw <spencer@memo-ict.nl>
2022-07-18 13:50:29 +0200

    add: credit card logo component

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -3,6 +3,7 @@ import './init/credit-card-components.init'
 import './extension/sw-flow-sequence-action';
 import './component/flow-sequence/action-order-ship-modal';
 import './component/flow-sequence/action-order-refund-modal';
+import './component/credit-card-logo'
 import './module/mollie-payments';
 import './module/sw-product';
 
