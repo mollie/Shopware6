1772210673416ac4bc80995ac739f259f5511190
Christian Dangl <boxblinkracer@gmx.net>
2022-08-02 08:25:40 +0200

    NTR: remove phpunit.autoload.php

--- phpunit.autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once __DIR__ . '/vendor/autoload.php';
-
-$classLoader = new \Composer\Autoload\ClassLoader();
-$classLoader->addPsr4("Kiener\\MolliePayments\\", __DIR__ . '/src', true);
-$classLoader->addPsr4("MolliePayments\\Tests\\", __DIR__ . '/tests/PHPUnit', true);
-$classLoader->register();
