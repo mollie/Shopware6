5d271bc98249f8670b15ccce223e65a253b84ff5
Daniel Schipper <daniel@memo-ict.nl>
2022-08-19 12:30:56 +0200

    Implement for Webhook events

--- src/Compatibility/Bundles/FlowBuilder/Events/WebhookReceivedEvent.php
+++ src/Compatibility/Bundles/FlowBuilder/Events/WebhookReceivedEvent.php
@@ -5,6 +5,7 @@ namespace Kiener\MolliePayments\Compatibility\Bundles\FlowBuilder\Events;
 use Kiener\MolliePayments\Service\Mollie\MolliePaymentStatus;
 use Shopware\Core\Checkout\Customer\CustomerDefinition;
 use Shopware\Core\Checkout\Customer\CustomerEntity;
+use Shopware\Core\Checkout\Order\Aggregate\OrderCustomer\OrderCustomerEntity;
 use Shopware\Core\Checkout\Order\OrderDefinition;
 use Shopware\Core\Checkout\Order\OrderEntity;
 use Shopware\Core\Framework\Context;
@@ -12,11 +13,14 @@ use Shopware\Core\Framework\Event\BusinessEventInterface;
 use Shopware\Core\Framework\Event\CustomerAware;
 use Shopware\Core\Framework\Event\EventData\EntityType;
 use Shopware\Core\Framework\Event\EventData\EventDataCollection;
+use Shopware\Core\Framework\Event\EventData\MailRecipientStruct;
 use Shopware\Core\Framework\Event\EventData\ScalarValueType;
+use Shopware\Core\Framework\Event\MailAware;
 use Shopware\Core\Framework\Event\OrderAware;
+use Shopware\Core\Framework\Event\SalesChannelAware;
 use Symfony\Contracts\EventDispatcher\Event;
 
-class WebhookReceivedEvent extends Event implements OrderAware, BusinessEventInterface
+class WebhookReceivedEvent extends Event implements OrderAware, MailAware, SalesChannelAware, BusinessEventInterface
 {
 
     /**
@@ -95,5 +99,23 @@ class WebhookReceivedEvent extends Event implements OrderAware, BusinessEventInt
     public function getContext(): Context
     {
         return $this->context;
+    }
+    
+    public function getMailStruct(): MailRecipientStruct
+    {
+        $customer = $this->order->getOrderCustomer();
+
+        if (!$customer instanceof OrderCustomerEntity) {
+            return new MailRecipientStruct([]);
+        }
+
+        return new MailRecipientStruct([
+            $customer->getEmail() => sprintf('%s %s', $customer->getFirstName(), $customer->getLastName()),
+        ]);
+    }
+
+    public function getSalesChannelId(): string
+    {
+        return $this->order->getSalesChannelId();
     }
 }
