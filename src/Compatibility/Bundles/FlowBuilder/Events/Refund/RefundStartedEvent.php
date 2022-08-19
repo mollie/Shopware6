abe5d6e29b3e2bc2e0c22536c863f02b2bd8dc30
Daniel Schipper <daniel@memo-ict.nl>
2022-08-19 12:18:58 +0200

    Implement MailAware for RefundStartedEvent

--- src/Compatibility/Bundles/FlowBuilder/Events/Refund/RefundStartedEvent.php
+++ src/Compatibility/Bundles/FlowBuilder/Events/Refund/RefundStartedEvent.php
@@ -2,17 +2,20 @@
 
 namespace Kiener\MolliePayments\Compatibility\Bundles\FlowBuilder\Events\Refund;
 
+use Shopware\Core\Checkout\Order\Aggregate\OrderCustomer\OrderCustomerEntity;
 use Shopware\Core\Checkout\Order\OrderDefinition;
 use Shopware\Core\Checkout\Order\OrderEntity;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Event\BusinessEventInterface;
 use Shopware\Core\Framework\Event\EventData\EntityType;
 use Shopware\Core\Framework\Event\EventData\EventDataCollection;
+use Shopware\Core\Framework\Event\EventData\MailRecipientStruct;
 use Shopware\Core\Framework\Event\EventData\ScalarValueType;
+use Shopware\Core\Framework\Event\MailAware;
 use Shopware\Core\Framework\Event\OrderAware;
 use Symfony\Contracts\EventDispatcher\Event;
 
-class RefundStartedEvent extends Event implements OrderAware, BusinessEventInterface
+class RefundStartedEvent extends Event implements OrderAware, MailAware, BusinessEventInterface
 {
 
     /**
@@ -90,5 +93,23 @@ class RefundStartedEvent extends Event implements OrderAware, BusinessEventInter
     public function getContext(): Context
     {
         return $this->context;
+    }
+
+    public function getMailStruct(): MailRecipientStruct
+    {
+        $customer = $this->order->getOrderCustomer();
+
+        if(!$customer instanceof OrderCustomerEntity) {
+            return new MailRecipientStruct([]);
+        }
+
+        return new MailRecipientStruct([
+            $customer->getEmail() => sprintf('%s %s', $customer->getFirstName(), $customer->getLastName())
+        ]);
+    }
+
+    public function getSalesChannelId(): ?string
+    {
+        return $this->order->getSalesChannelId();
     }
 }
