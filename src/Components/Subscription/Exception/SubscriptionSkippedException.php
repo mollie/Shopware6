b1872d9f03bae4e89cc5a849509d227ecf455200
Christian <boxblinkracer@gmx.net>
2022-09-02 07:47:53 +0200

    MOL-892: Add option to skip subscription renewal with failed payment (#414)

---
+++ src/Components/Subscription/Exception/SubscriptionSkippedException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kiener\MolliePayments\Components\Subscription\Exception;
+
+class SubscriptionSkippedException extends \Exception
+{
+
+    /**
+     * @param string $subscriptionId
+     * @param string $transactionId
+     * @param null|\Throwable $previous
+     */
+    public function __construct(string $subscriptionId, string $transactionId, ?\Throwable $previous = null)
+    {
+        $message = 'Skip renewal of subscription ' . $subscriptionId . '. Incoming transaction ' . $transactionId . ' of Mollie is not successfully paid';
+
+        parent::__construct($message, 0, $previous);
+    }
+}
