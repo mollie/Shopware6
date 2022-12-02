d03ea146d065a4030eaeb348a8d2f21904b1e4a1
Christian Dangl <boxblinkracer@gmx.net>
2022-12-02 23:55:40 +0100

    MOL-984: Fix plugin config custom format for Shopware 6.4.7.0

--- src/Resources/app/administration/src/module/mollie-payments/components/mollie-pluginconfig-section-payments-format/index.js
+++ src/Resources/app/administration/src/module/mollie-payments/components/mollie-pluginconfig-section-payments-format/index.js
@@ -1,6 +1,6 @@
 import template from './mollie-pluginconfig-section-payments-format.html.twig';
 import './mollie-pluginconfig-section-payments-format.scss';
-import StringUtils from "../../../../core/service/utils/string-utils.service";
+import StringUtils from '../../../../core/service/utils/string-utils.service';
 
 // eslint-disable-next-line no-undef
 const {Component} = Shopware;
@@ -44,24 +44,43 @@ Component.register('mollie-pluginconfig-section-payments-format', {
          * @returns {string|*}
          */
         getFormat(ordernumber) {
-            const configRoot = this.$parent.$parent.$parent.$parent.$parent;
 
-            if (configRoot === null) {
+            const actualConfigData = this.getConfigData();
+
+            if (actualConfigData === undefined || actualConfigData === null) {
                 return '';
             }
 
-            // does not exist in shopware 6.3.5.2
-            if (configRoot.actualConfigData === undefined || configRoot.actualConfigData === null) {
-                return '';
-            }
-
-            var text = configRoot.actualConfigData.null['MolliePayments.config.formatOrderNumber'];
+            var text = actualConfigData.null['MolliePayments.config.formatOrderNumber'];
 
             const stringUtils = new StringUtils();
             text = stringUtils.replace('{ordernumber}', ordernumber, text);
 
             return text;
         },
+
+        /**
+         *
+         * @returns {*}
+         */
+        getConfigData() {
+            // Shopware > 6.4.7.0
+            const configRootNew = this.$parent.$parent.$parent.$parent.$parent;
+            // Shopware <= 6.4.7.0
+            const configRootOld = this.$parent.$parent.$parent.$parent;
+
+            if (configRootNew !== null && configRootNew.actualConfigData !== undefined) {
+                return configRootNew.actualConfigData;
+            }
+
+            if (configRootOld !== null && configRootOld.actualConfigData !== undefined) {
+                return configRootOld.actualConfigData;
+            }
+
+            // does not exist e.g. in Shopware 6.3.5.2
+            return null;
+        },
+
     },
 
 });
