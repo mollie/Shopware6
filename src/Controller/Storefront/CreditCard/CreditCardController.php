4904c9f415479dee4d8c429e9ce063eecb6308ce
Vitalij Mik <BlackScorp@users.noreply.github.com>
2022-12-09 08:30:11 +0100

    MOL-965: Fix success in storeCardToken Response (#470)

--- src/Controller/Storefront/CreditCard/CreditCardController.php
+++ src/Controller/Storefront/CreditCard/CreditCardController.php
@@ -3,6 +3,7 @@
 namespace Kiener\MolliePayments\Controller\Storefront\CreditCard;
 
 use Kiener\MolliePayments\Service\CustomerService;
+use Kiener\MolliePayments\Service\CustomerServiceInterface;
 use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
@@ -17,15 +18,15 @@ class CreditCardController extends StorefrontController
 {
 
     /**
-     * @var CustomerService
+     * @var CustomerServiceInterface
      */
     private $customerService;
 
 
     /**
-     * @param CustomerService $customerService
+     * @param CustomerServiceInterface $customerService
      */
-    public function __construct(CustomerService $customerService)
+    public function __construct(CustomerServiceInterface $customerService)
     {
         $this->customerService = $customerService;
     }
@@ -42,7 +43,7 @@ class CreditCardController extends StorefrontController
     public function storeCardToken(SalesChannelContext $context, string $customerId, string $cardToken): JsonResponse
     {
         $result = null;
-
+        $success = false;
         /** @var CustomerEntity $customer */
         $customer = $this->customerService->getCustomer($customerId, $context->getContext());
 
@@ -52,11 +53,13 @@ class CreditCardController extends StorefrontController
                 $cardToken,
                 $context->getContext()
             );
-            $result = $writtenEvent->getErrors();
+            $errors = $writtenEvent->getErrors();
+            $success = count($errors) === 0;
+            $result = $errors;
         }
 
         return new JsonResponse([
-            'success' => (bool)$result,
+            'success' => $success,
             'customerId' => $customerId,
             'result' => $result
         ]);
