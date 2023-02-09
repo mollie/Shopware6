1c33680e003cba7686527f6b303a30cafbee9fdf
Christian <boxblinkracer@gmx.net>
2023-02-09 09:02:12 +0100

    MOL-1090: fix wrong url anonymizer (#505)

--- src/Service/Logger/Services/URLAnonymizer.php
+++ src/Service/Logger/Services/URLAnonymizer.php
@@ -14,7 +14,9 @@ class URLAnonymizer implements URLAnonymizerInterface
         # we do not want to save the used tokens in our log file
         # also, those one time tokens are soooooo long. it's just annoying and fills disk space ;)
         if (strpos($url, '/payment/finalize-transaction') !== false) {
-            $url = substr($url, 0, 80);
+            $parts = explode('/payment/finalize-transaction', $url);
+
+            $url = str_replace($parts[1], '', $url);
         }
 
         return (string)$url;
