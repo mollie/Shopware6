0833ec62e8ba978f05121ea458fb6c6e3bab78b9
Smemoict <spencer@memo-ict.nl>
2023-02-09 15:23:46 +0100

    cha: formatted migration

--- src/Migration/Migration1669796049CreateRefund.php
+++ src/Migration/Migration1669796049CreateRefund.php
@@ -5,11 +5,14 @@ namespace Kiener\MolliePayments\Migration;
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Migration\MigrationStep;
 
+
 class Migration1669796049CreateRefund extends MigrationStep
 {
+
     /**
      * @return int
      */
+
     public function getCreationTimestamp(): int
     {
         return 1669796049;
@@ -17,22 +20,23 @@ class Migration1669796049CreateRefund extends MigrationStep
 
     /**
      * @param Connection $connection
-     * @throws \Doctrine\DBAL\Exception
      * @return void
+     * @throws \Doctrine\DBAL\Exception
      */
     public function update(Connection $connection): void
     {
         $connection->exec(
-            "CREATE TABLE IF NOT EXISTS `mollie_refund` (
-    `id` BINARY(16) NOT NULL,
-    `order_id` BINARY(16) NULL,
-    `mollie_refund_id` VARCHAR(255) NULL,
-    `public_description` LONGTEXT NULL,
-    `internal_description` LONGTEXT NULL,
-    `created_at` DATETIME(3) NOT NULL,
-    `updated_at` DATETIME(3) NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+            "
+                CREATE TABLE IF NOT EXISTS `mollie_refund` (
+                  `id` BINARY(16) NOT NULL,
+                  `order_id` BINARY(16) NULL,
+                  `mollie_refund_id` VARCHAR(255) NULL,
+                  `public_description` LONGTEXT NULL,
+                  `internal_description` LONGTEXT NULL,
+                  `created_at` DATETIME(3) NOT NULL,
+                  `updated_at` DATETIME(3) NULL,
+                  PRIMARY KEY (`id`)
+                ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
                 "
         );
     }
