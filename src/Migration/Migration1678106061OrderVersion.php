39e5e16722e13bd00adde7759b84c936f421315f
Vitalij Mik <BlackScorp@users.noreply.github.com>
2023-03-09 12:47:45 +0100

    MOL-1102: Fix migration (#528)
    Co-authored-by: Vitalij Mik <vitalij.mik@dasistweb.de>

--- src/Migration/Migration1678106061OrderVersion.php
+++ src/Migration/Migration1678106061OrderVersion.php
@@ -19,14 +19,14 @@ class Migration1678106061OrderVersion extends MigrationStep
 ALTER TABLE `mollie_refund`
 ADD COLUMN `order_version_id` BINARY(16) NULL DEFAULT NULL AFTER `order_id`
 SQL;
-        $connection->executeStatement($sql);
+        $connection->exec($sql);
 
         $sql = <<<SQL
 UPDATE `mollie_refund`
 SET `order_version_id` = :orderVersionId
 SQL;
 
-        $connection->executeStatement($sql, ['orderVersionId' => Defaults::LIVE_VERSION]);
+        $connection->exec($sql, ['orderVersionId' => Defaults::LIVE_VERSION]);
 
         $sql = <<<SQL
 ALTER TABLE `mollie_refund`
@@ -34,7 +34,7 @@ CHANGE `order_version_id` `order_version_id` BINARY(16) NOT NULL,
 ADD KEY `fk.mollie_refund.order_id` (`order_id`,`order_version_id`),
 ADD CONSTRAINT `fk.mollie_refund.order_id` FOREIGN KEY (`order_id`,`order_version_id`) REFERENCES `order` (`id`,`version_id`) ON DELETE CASCADE ON UPDATE CASCADE
 SQL;
-        $connection->executeStatement($sql);
+        $connection->exec($sql);
     }
 
     public function updateDestructive(Connection $connection): void
