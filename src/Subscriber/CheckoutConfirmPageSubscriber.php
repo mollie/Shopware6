48bbb5146ed1d921d00d8c7f2f6badd1546e9878
smemoict <101330447+smemoict@users.noreply.github.com>
2023-04-03 15:53:54 +0200

    MOL-877: Compatibility for Shopware 6.5 - Locale Repository (#552)
    * add: locale.repository
    
    * fix: stan/phpunit/interface
    
    ---------
    
    Co-authored-by: Christian <boxblinkracer@gmx.net>

--- src/Subscriber/CheckoutConfirmPageSubscriber.php
+++ src/Subscriber/CheckoutConfirmPageSubscriber.php
@@ -5,6 +5,8 @@ namespace Kiener\MolliePayments\Subscriber;
 use Exception;
 use Kiener\MolliePayments\Factory\MollieApiFactory;
 use Kiener\MolliePayments\Handler\Method\CreditCardPayment;
+use Kiener\MolliePayments\Repository\Locale\LocaleRepository;
+use Kiener\MolliePayments\Repository\Locale\LocaleRepositoryInterface;
 use Kiener\MolliePayments\Service\CustomerService;
 use Kiener\MolliePayments\Service\CustomerServiceInterface;
 use Kiener\MolliePayments\Service\CustomFieldService;
@@ -57,7 +59,7 @@ class CheckoutConfirmPageSubscriber implements EventSubscriberInterface
     private $languageRepositoryInterface;
 
     /**
-     * @var EntityRepositoryInterface
+     * @var LocaleRepositoryInterface
      */
     private $localeRepositoryInterface;
 
@@ -83,15 +85,15 @@ class CheckoutConfirmPageSubscriber implements EventSubscriberInterface
      * @param MollieApiFactory $apiFactory
      * @param SettingsService $settingsService
      * @param EntityRepositoryInterface $languageRepositoryInterface
-     * @param EntityRepositoryInterface $localeRepositoryInterface
+     * @param LocaleRepositoryInterface $localeRepositoryInterface
      * @param MandateServiceInterface $mandateService
      */
     public function __construct(
-        MollieApiFactory $apiFactory,
-        SettingsService $settingsService,
+        MollieApiFactory          $apiFactory,
+        SettingsService           $settingsService,
         EntityRepositoryInterface $languageRepositoryInterface,
-        EntityRepositoryInterface $localeRepositoryInterface,
-        MandateServiceInterface $mandateService
+        LocaleRepositoryInterface $localeRepositoryInterface,
+        MandateServiceInterface   $mandateService
     ) {
         $this->apiFactory = $apiFactory;
         $this->settingsService = $settingsService;
