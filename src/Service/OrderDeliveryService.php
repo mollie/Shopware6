0b71cce77d053fef3f773b5591ec1a29b063172b
smemoict <101330447+smemoict@users.noreply.github.com>
2023-04-03 16:55:14 +0200

    MOL-877: Compatibility for Shopware 6.5 -  Order Delivery Repository (#549)
    * add: order_delivery.repository
    
    * fix: name error
    
    * fix:stan/unittest/interface
    
    ---------
    
    Co-authored-by: Christian <boxblinkracer@gmx.net>

--- src/Service/OrderDeliveryService.php
+++ src/Service/OrderDeliveryService.php
@@ -2,19 +2,19 @@
 
 namespace Kiener\MolliePayments\Service;
 
+use Kiener\MolliePayments\Repository\OrderDelivery\OrderDeliveryRepositoryInterface;
 use Shopware\Core\Checkout\Order\Aggregate\OrderDelivery\OrderDeliveryEntity;
 use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 
 class OrderDeliveryService
 {
     /**
-     * @var EntityRepositoryInterface
+     * @var OrderDeliveryRepositoryInterface
      */
     private $orderDeliveryRepository;
 
-    public function __construct(EntityRepositoryInterface $orderDeliveryRepository)
+    public function __construct(OrderDeliveryRepositoryInterface $orderDeliveryRepository)
     {
         $this->orderDeliveryRepository = $orderDeliveryRepository;
     }
