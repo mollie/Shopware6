df7f863a0f2b6c6662680857ae17c88f86fcb984
Christian Dangl <boxblinkracer@gmx.net>
2023-05-26 13:01:59 +0200

    MOL-1159: Revert Fix Apple Pay Direct saluation in Shopware 6.4
    This reverts commit b49ca119bc0370201ae01e0e7879d01bb17d8cb2.

--- src/Service/CustomerService.php
+++ src/Service/CustomerService.php
@@ -3,7 +3,6 @@
 namespace Kiener\MolliePayments\Service;
 
 use Exception;
-use Kiener\MolliePayments\Compatibility\VersionCompare;
 use Kiener\MolliePayments\Exception\CouldNotCreateMollieCustomerException;
 use Kiener\MolliePayments\Exception\CouldNotFetchMollieCustomerException;
 use Kiener\MolliePayments\Exception\CustomerCouldNotBeFoundException;
@@ -20,7 +19,6 @@ use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Customer\Event\CustomerBeforeLoginEvent;
 use Shopware\Core\Checkout\Customer\Event\CustomerLoginEvent;
 use Shopware\Core\Checkout\Order\Aggregate\OrderAddress\OrderAddressEntity;
-use Shopware\Core\Defaults;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenContainerEvent;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
@@ -506,16 +504,7 @@ class CustomerService implements CustomerServiceInterface
     {
         try {
             $criteria = new Criteria();
-
-            $versionCompare = new VersionCompare($this->shopwareVersion);
-
-            if ($versionCompare->gte('6.5.0.0')) {
-                $criteria->addFilter(new EqualsFilter('salutationKey', 'not_specified'));
-            } else {
-                # in Shopware 6.4.x, it seems that not_specified is not allowed in the AddressValidator->isValidSalutationId.
-                # So we unfortunately have to use "mr" as a fallback.
-                $criteria->addFilter(new EqualsFilter('salutationKey', 'mr'));
-            }
+            $criteria->addFilter(new EqualsFilter('salutationKey', 'not_specified'));
 
             // Get salutations
             /** @var string[] $salutations */
