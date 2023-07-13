0bacbe13e61a90d2c309c9ac82b26fffde01ac4f
Christian Dangl <boxblinkracer@gmx.net>
2023-07-13 14:57:00 +0200

    MOL-1197: shopware removed static mollie js file after building

--- src/Resources/app/storefront/webpack.config.js
+++ src/Resources/app/storefront/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: 'production',
     entry: './src/custom.js',
     output: {
-        path: path.resolve(__dirname, '..', '..', 'public', 'static', 'js'),
+        path: path.resolve(__dirname, '..', '..', 'public'),
         filename: 'mollie-payments.js',
     },
     resolve: {
