baa3b2498fd191ad9338d937be54b8d38b170018
Christian Dangl <cd@dasistweb.de>
2023-08-01 13:49:18 +0200

    NTR: fix cypress test for business events

--- tests/Cypress/cypress/support/actions/admin/AdminSettingsAction.js
+++ tests/Cypress/cypress/support/actions/admin/AdminSettingsAction.js
@@ -1,25 +1,19 @@
-import Shopware from "Services/shopware/Shopware";
-
-const shopware = new Shopware();
-
-
 export default class AdminSettingsAction {
 
     /**
      *
      */
     openBusinessEventsPage() {
-
         cy.visit('/admin#/sw/settings/index');
-        cy.wait(4000);
         cy.get('a#sw-event-action').click();
-        cy.wait(3000);
     }
 
-    openFirstBusinessEvent(){
-        cy.get('.sw-data-grid__body .sw-data-grid__row.sw-data-grid__row--0 .sw-context-button__button').click({force:true});
-        cy.get('.sw-context-menu-item.sw-entity-listing__context-menu-edit-action').click();
+    /**
+     *
+     */
+    openFirstBusinessEvent() {
+        cy.get('.sw-data-grid__body .sw-data-grid__row.sw-data-grid__row--0 .sw-context-button__button').click({force: true});
+        cy.get('.sw-context-menu-item.sw-entity-listing__context-menu-edit-action').click({force: true});
     }
-
 
 }
