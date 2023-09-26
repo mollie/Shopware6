486ff9a7f737bcb51fc26c5fefd82771b0fcf0a2
Christian Dangl <cd@dasistweb.de>
2023-09-26 13:21:05 +0200

    MOL-1250: add cypress test for TWINT

---
+++ tests/Cypress/cypress/e2e/storefront/payment-methods/twint.cy.js
@@ -0,0 +1,43 @@
+import Devices from "Services/utils/Devices";
+import Session from "Services/utils/Session"
+// ------------------------------------------------------
+import PaymentAction from "Actions/storefront/checkout/PaymentAction";
+import DummyBasketScenario from "Scenarios/DummyBasketScenario";
+// ------------------------------------------------------
+
+
+const devices = new Devices();
+const session = new Session();
+
+const paymentAction = new PaymentAction();
+const scenarioDummyBasket = new DummyBasketScenario(1);
+
+const device = devices.getFirstDevice();
+
+
+describe('TWINT', () => {
+
+    context(devices.getDescription(device), () => {
+
+        before(function () {
+            devices.setDevice(device);
+        })
+
+        beforeEach(() => {
+            session.resetBrowserSession();
+            devices.setDevice(device);
+        });
+
+        it('C1628203: TWINT is existing in checkout', () => {
+
+            scenarioDummyBasket.execute();
+
+            paymentAction.switchPaymentMethod('TWINT');
+
+            // payment would only work using currency CHF which cannot be done at the moment
+        })
+
+    })
+
+})
+
