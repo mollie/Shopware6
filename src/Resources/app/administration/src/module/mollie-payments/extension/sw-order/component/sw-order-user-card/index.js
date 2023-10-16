d38cdcce9e89fbf76f4d6f422121146c1cef42d1
Christian Dangl <cd@dasistweb.de>
2023-10-16 17:48:45 +0200

    MOL-1244: fix refund manager for swag commercial plugin

--- src/Resources/app/administration/src/module/mollie-payments/extension/sw-order/component/sw-order-user-card/index.js
+++ src/Resources/app/administration/src/module/mollie-payments/extension/sw-order/component/sw-order-user-card/index.js
@@ -2,7 +2,6 @@ import template from './sw-order-user-card.html.twig';
 import './sw-order-user-card.scss';
 import OrderAttributes from '../../../../../../core/models/OrderAttributes';
 
-
 // eslint-disable-next-line no-undef
 const {Component} = Shopware;
 
@@ -20,6 +19,24 @@ Component.override('sw-order-user-card', {
     },
 
     computed: {
+
+        /**
+         *
+         * @returns {boolean}
+         */
+        isMollieOrder() {
+            const attr = new OrderAttributes(this.currentOrder);
+            return attr.isMollieOrder();
+        },
+
+        /**
+         *
+         * @returns {*}
+         */
+        hasCreditCardData() {
+            return this._creditCardData().hasCreditCardData();
+        },
+
         /**
          *
          * @returns {string|*}
@@ -49,30 +66,17 @@ Component.override('sw-order-user-card', {
          * @returns {null|string|*}
          */
         mollieOrderId() {
-
             const orderAttributes = new OrderAttributes(this.currentOrder);
-
-            if (orderAttributes.getOrderId() !== '') {
-                return orderAttributes.getOrderId();
-            }
-
-            if (orderAttributes.getPaymentId() !== '') {
-                return orderAttributes.getPaymentId();
-            }
-
-            return null;
+            return orderAttributes.getMollieID();
         },
-        mollieThirdPartyPaymentId() {
-            if (
-                !!this.currentOrder
-                && !!this.currentOrder.customFields
-                && !!this.currentOrder.customFields.mollie_payments
-                && !!this.currentOrder.customFields.mollie_payments.third_party_payment_id
-            ) {
-                return this.currentOrder.customFields.mollie_payments.third_party_payment_id;
-            }
 
-            return null;
+        /**
+         *
+         * @returns {string}
+         */
+        mollieThirdPartyPaymentId() {
+            const orderAttributes = new OrderAttributes(this.currentOrder);
+            return orderAttributes.getPaymentRef();
         },
 
         /**
@@ -81,7 +85,7 @@ Component.override('sw-order-user-card', {
          */
         isSubscription() {
             const orderAttributes = new OrderAttributes(this.currentOrder);
-            return (orderAttributes.getSwSubscriptionId() !== '');
+            return orderAttributes.isSubscription();
         },
 
         /**
@@ -101,10 +105,6 @@ Component.override('sw-order-user-card', {
             return this.molliePaymentUrl !== '';
         },
 
-        hasCreditCardData() {
-            return this._creditCardData().hasCreditCardData();
-        },
-
     },
 
     created() {
@@ -114,6 +114,29 @@ Component.override('sw-order-user-card', {
     methods: {
         /**
          *
+         */
+        createdComponent() {
+            this.$super('createdComponent');
+
+            this.molliePaymentUrl = '';
+
+            if (!this.mollieOrderId) {
+                return;
+            }
+
+            this.isMolliePaymentUrlLoading = true;
+
+            this.MolliePaymentsOrderService.getPaymentUrl({orderId: this.currentOrder.id})
+                .then(response => {
+                    this.molliePaymentUrl = (response.url !== null) ? response.url : '';
+                })
+                .finally(() => {
+                    this.isMolliePaymentUrlLoading = false;
+                });
+        },
+
+        /**
+         *
          * @returns {CreditcardAttributes|*}
          */
         _creditCardData() {
@@ -121,54 +144,21 @@ Component.override('sw-order-user-card', {
             return orderAttributes.getCreditCardAttributes();
         },
 
-        createdComponent() {
-            this.$super('createdComponent');
-
-            this.molliePaymentUrl = '';
-
-            if (this.mollieOrderId) {
-                this.isMolliePaymentUrlLoading = true;
-
-                this.MolliePaymentsOrderService.getPaymentUrl({orderId: this.currentOrder.id})
-                    .then(response => {
-                        this.molliePaymentUrl = (response.url !== null) ? response.url : '';
-                    })
-                    .finally(() => {
-                        this.isMolliePaymentUrlLoading = false;
-                    });
-            }
-        },
-
+        /**
+         *
+         */
         copyPaymentUrlToClipboard() {
             // eslint-disable-next-line no-undef
             Shopware.Utils.dom.copyToClipboard(this.molliePaymentUrl);
             this.molliePaymentUrlCopied = true;
         },
 
-        onMolliePaymentUrlProcessFinished(value) {
-            this.molliePaymentUrlCopied = value;
-        },
-
         /**
          *
-         * @returns {{voucher_type: *}|*|null}
+         * @param value
          */
-        getMollieData() {
-            if (this.currentOrder === undefined || this.currentOrder === null) {
-                return null;
-            }
-
-            if (this.currentOrder.customFields === undefined || this.currentOrder.customFields === null) {
-                return null;
-            }
-
-            const customFields = this.currentOrder.customFields;
-
-            if (customFields.mollie_payments === undefined || customFields.mollie_payments === null) {
-                return null;
-            }
-
-            return customFields.mollie_payments;
+        onMolliePaymentUrlProcessFinished(value) {
+            this.molliePaymentUrlCopied = value;
         },
 
     },
