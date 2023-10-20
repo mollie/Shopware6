315f7a0d16a6d001431a483a97639e1bdace6cf6
Vitalij Mik <BlackScorp@users.noreply.github.com>
2023-10-20 08:16:22 +0200

    MOL-1124: do not load settings without db (#644)
    * MOL-1124: do not load settings without db
    
    * MOL-1124: code style
    
    * MOL-1124: CS Fix
    
    ---------
    
    Co-authored-by: Vitalij Mik <vitalij.mik@dasistweb.de>

--- src/Service/Logger/MollieLoggerFactory.php
+++ src/Service/Logger/MollieLoggerFactory.php
@@ -36,17 +36,24 @@ class MollieLoggerFactory
      */
     private $retentionDays;
 
+    /**
+     * @var string
+     */
+    private $dsn;
+
 
     /**
      * @param SettingsService $settingsService
      * @param string $filename
      * @param string $retentionDays
+     * @param string $dsn
      */
-    public function __construct(SettingsService $settingsService, string $filename, string $retentionDays)
+    public function __construct(SettingsService $settingsService, string $filename, string $retentionDays, string $dsn)
     {
         $this->settingsService = $settingsService;
         $this->filename = $filename;
         $this->retentionDays = $retentionDays;
+        $this->dsn = $dsn;
     }
 
     /**
@@ -54,6 +61,11 @@ class MollieLoggerFactory
      */
     public function createLogger(): LoggerInterface
     {
+        if ($this->dsn === '' || $this->dsn === 'mysql://_placeholder.test') {
+            // deployment server without database
+            return new Logger(self::CHANNEL);
+        }
+
         $config = $this->settingsService->getSettings();
 
         # 100 = DEBUG, 200 = INFO
