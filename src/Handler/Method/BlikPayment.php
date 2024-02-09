1914194d9e935d59b6a342e4e2e327abb89f327e
Vitalij Mik <BlackScorp@users.noreply.github.com>
2024-02-09 14:59:16 +0100

    NTR: PIHPS-205 - add new payment method blik (#701)
    * NTR: PIHPS-205 - add new payment method blik
    
    * NTR: merge master
    
    commit 3a0f05dc025ae4733363b2548726b7290b159ed6
    Author: Vitalij Mik <BlackScorp@users.noreply.github.com>
    Date:   Fri Feb 9 12:28:17 2024 +0100
    
        NTR: test SW 6.5.8.4 (#703)
    
        Co-authored-by: Vitalij Mik <vitalij.mik@dasistweb.de>
    
    commit f8390b387f3ce8fb6a894d32470bb2a24414b3e4
    Author: Vitalij Mik <BlackScorp@users.noreply.github.com>
    Date:   Fri Feb 9 11:29:47 2024 +0100
    
        NTR: new shopware version (#702)
    
        Co-authored-by: Vitalij Mik <vitalij.mik@dasistweb.de>
    
    * NTR: set billing address from cart
    
    * NTR: change text
    
    * NTR: change name
    
    ---------
    
    Co-authored-by: Vitalij Mik <vitalij.mik@dasistweb.de>

---
+++ src/Handler/Method/BlikPayment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Kiener\MolliePayments\Handler\Method;
+
+use Kiener\MolliePayments\Handler\PaymentHandler;
+use Mollie\Api\Types\PaymentMethod;
+use Shopware\Core\Checkout\Customer\CustomerEntity;
+use Shopware\Core\Checkout\Order\OrderEntity;
+use Shopware\Core\System\Locale\LocaleEntity;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+
+class BlikPayment extends PaymentHandler
+{
+    public const PAYMENT_METHOD_NAME = 'blik';
+    public const PAYMENT_METHOD_DESCRIPTION = 'Blik';
+
+    /** @var string */
+    protected $paymentMethod = self::PAYMENT_METHOD_NAME;
+
+
+    /**
+     * @param array<mixed> $orderData
+     * @param OrderEntity $orderEntity
+     * @param SalesChannelContext $salesChannelContext
+     * @param CustomerEntity $customer
+     * @return array<mixed>
+     */
+    public function processPaymentMethodSpecificParameters(array $orderData, OrderEntity $orderEntity, SalesChannelContext $salesChannelContext, CustomerEntity $customer): array
+    {
+        return $orderData;
+    }
+}
