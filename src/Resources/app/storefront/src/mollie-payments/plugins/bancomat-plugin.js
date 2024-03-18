1699a4a643b78fb5096eef41f7831c2307948e9b
Vitalij Mik <vitalij.mik@dasistweb.de>
2024-03-18 15:32:30 +0100

    NTR: remove console.log

--- src/Resources/app/storefront/src/mollie-payments/plugins/bancomat-plugin.js
+++ src/Resources/app/storefront/src/mollie-payments/plugins/bancomat-plugin.js
@@ -6,19 +6,18 @@ export default class MollieBancomatPlugin extends Plugin {
 
         const phoneField = document.getElementById('mollieBancomatPayPhone');
         if (phoneField === null) {
-            console.log("phone field not found");
+
             return;
         }
 
         const inputFieldWrapper = document.querySelector('.mollie-bancomat-pay');
-        console.log(inputFieldWrapper);
+
         const errorMessageElement = document.querySelector('.mollie-bancomat-pay [data-form-validation-invalid-phone="true"]');
-        console.log(errorMessageElement);
+
         phoneField.addEventListener('focus',(e)=>{
             inputFieldWrapper.classList.add('was-validated');
             e.target.removeAttribute('invalid');
             errorMessageElement.classList.add('d-none');
-            console.log("focus");
         });
 
 
@@ -28,7 +27,6 @@ export default class MollieBancomatPlugin extends Plugin {
                 e.target.setAttribute('invalid',true);
                 errorMessageElement.classList.remove('d-none');
             }
-            console.log("blur");
             return form.reportValidity();
 
         })
