00b9ae3327f7b437c38170df6e5bfe228c952ffb
Vitalij Mik <vitalij.mik@dasistweb.de>
2024-05-02 15:33:20 +0200

    NTR: fix URL encoding for filenames

--- src/Service/MollieApi/LineItemDataExtractor.php
+++ src/Service/MollieApi/LineItemDataExtractor.php
@@ -29,7 +29,9 @@ class LineItemDataExtractor
             && $medias->first() instanceof ProductMediaEntity
             && $medias->first()->getMedia() instanceof MediaEntity
         ) {
-            $extraData->setImageUrl($medias->first()->getMedia()->getUrl());
+            $url = $medias->first()->getMedia()->getUrl();
+            $url = $this->encodePathAndQuery($url);
+            $extraData->setImageUrl($url);
         }
 
         $seoUrls = $product->getSeoUrls();
@@ -40,5 +42,39 @@ class LineItemDataExtractor
         }
 
         return $extraData;
+    }
+
+    private function encodePathAndQuery(string $fullUrl):string
+    {
+        $urlParts = parse_url($fullUrl);
+
+        $scheme = isset($urlParts['scheme']) ? $urlParts['scheme'] . '://' : '';
+
+        $host = isset($urlParts['host']) ? $urlParts['host'] : '';
+
+        $port = isset($urlParts['port']) ? ':' . $urlParts['port'] : '';
+
+        $user = isset($urlParts['user']) ? $urlParts['user'] : '';
+
+        $pass = isset($urlParts['pass']) ? ':' . $urlParts['pass']  : '';
+
+        $pass = ($user || $pass) ? "$pass@" : '';
+
+        $path = isset($urlParts['path']) ? $urlParts['path'] : '';
+
+        if (mb_strlen($path) > 0) {
+            $pathParts = explode('/', $path);
+            array_walk($pathParts, function (&$pathPart) {
+                $pathPart = rawurlencode($pathPart);
+            });
+            $path = implode('/', $pathParts);
+        }
+
+        $query = isset($urlParts['query']) ? '?' . $urlParts['query'] : '';
+
+
+        $fragment = isset($urlParts['fragment']) ? '#' . $urlParts['fragment'] : '';
+
+        return trim($scheme.$user.$pass.$host.$port.$path.$query.$fragment);
     }
 }
