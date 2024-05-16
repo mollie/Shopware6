5266d010f62358db633bb84d548369bac9680801
Vitalij Mik <vitalij.mik@dasistweb.de>
2024-05-16 11:00:59 +0200

    NTR: fix column

--- src/Migration/MigrationUtils.php
+++ src/Migration/MigrationUtils.php
@@ -68,7 +68,7 @@ class MigrationUtils
 
     public function columnExists(string $table, string $column): bool
     {
-        return $this->connection->executeQuery("SHOW COLUMNS FROM " . $table . " LIKE '" . $column . "'")->fetchOne();
+        return (bool)$this->connection->executeQuery("SHOW COLUMNS FROM " . $table . " LIKE '" . $column . "'")->fetchOne();
     }
 
     /**
