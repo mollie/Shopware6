82bb328702842f528e95b3e9f72ee766ed16d51c
m-muxfeld-diw <143803170+m-muxfeld-diw@users.noreply.github.com>
2024-06-28 13:19:07 +0200

    PISHPS-293: copy to clipboard fix for shopware 6.6.2.0 (#767)
    * PISHPS-282: added openapi v3 documentation for store-api and admin-api
    
    * PISHPS-282: removed /api/ and /store-api from routes, documented credit-card endpoints and renamed directory
    
    * PISHPS-282: removed duplicated openapi v3 json files
    
    * PISHPS-293: added fallback for Shopware.Utils.dom.copyToClipboard if unavailable

--- src/Resources/app/administration/src/module/mollie-payments/extension/sw-order/view/sw-order-detail-general/index.js
+++ src/Resources/app/administration/src/module/mollie-payments/extension/sw-order/view/sw-order-detail-general/index.js
@@ -221,12 +221,20 @@ Component.override('sw-order-detail-general', {
             return orderAttributes.getCreditCardAttributes();
         },
 
+
+
         /**
          *
          */
         copyPaymentUrlToClipboard() {
+            let fallback = async function(e) {
+                await navigator.clipboard.writeText(e)
+            };
+
             // eslint-disable-next-line no-undef
-            Shopware.Utils.dom.copyToClipboard(this.molliePaymentUrl);
+            let clipboard = typeof Shopware.Utils.dom.copyToClipboard === 'function' ? Shopware.Utils.dom.copyToClipboard : fallback;
+            // eslint-disable-next-line no-undef
+            clipboard(this.molliePaymentUrl);
             this.molliePaymentUrlCopied = true;
         },
 
