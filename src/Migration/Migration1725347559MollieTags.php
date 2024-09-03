855e75aaa3ef11b6a7ccafa89c0068141419287c
Marvin Muxfeld <marvin.muxfeld@dasistweb.de>
2024-09-03 10:46:22 +0200

    PISHPS-304: added OrderTagService

---
+++ src/Migration/Migration1725347559MollieTags.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace Kiener\MolliePayments\Migration;
+
+use Doctrine\DBAL\Connection;
+use Kiener\MolliePayments\Struct\Tags\AbstractTag;
+use Kiener\MolliePayments\Struct\Tags\SubscriptionTag;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\Migration\MigrationStep;
+use Shopware\Core\Framework\Uuid\Uuid;
+
+/**
+ * @internal
+ */
+#[Package('core')]
+class Migration1725347559MollieTags extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1725347559;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $tags = [SubscriptionTag::create()];
+
+        foreach ($tags as $tag) {
+            /** @var $tag AbstractTag */
+            $this->createTag($connection, $tag->getId(), $tag->getName());
+        }
+    }
+
+    /**
+     * @param Connection $connection
+     * @return void
+     */
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+
+    private function createTag(
+        Connection $connection,
+        string $id,
+        string $name
+    ): void {
+        // Create a new QueryBuilder instance
+        $queryBuilder = $connection->createQueryBuilder();
+
+        // Build the INSERT query
+        $queryBuilder
+            ->insert('tag')
+            ->values([
+                'id' => ':id',
+                'name' => ':name',
+                'created_at' => ':created_at',
+                'updated_at' => ':updated_at',
+            ])
+            ->setParameters([
+                'id' => Uuid::fromHexToBytes($id),
+                'name' => $name,
+                'created_at' => (new \DateTime())->format('Y-m-d H:i:s'), // current timestamp
+                'updated_at' => null,
+            ]);
+
+        /** @phpstan-ignore-next-line for what ever reason stan says this line doesn't exist, but it does */
+        $queryBuilder->executeStatement();
+    }
+}
---
+++ src/Service/Tags/OrderTagService.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+
+namespace Kiener\MolliePayments\Service\Tags;
+
+use Closure;
+use Kiener\MolliePayments\Components\Subscription\DAL\Subscription\SubscriptionEntity;
+use Kiener\MolliePayments\Service\Tags\Exceptions\CouldNotTagOrderException;
+use Kiener\MolliePayments\Struct\Tags\SubscriptionTag;
+use Shopware\Core\Checkout\Order\OrderEntity;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\System\Tag\TagCollection;
+use Shopware\Core\System\Tag\TagEntity;
+
+class OrderTagService
+{
+    /**
+     * @var EntityRepository
+     */
+    private $orderRepository;
+
+    /**
+     * @var EntityRepository
+     */
+    private $tagRepository;
+
+    public function __construct(
+        EntityRepository $orderRepository,
+        EntityRepository $tagRepository
+    ) {
+        $this->orderRepository = $orderRepository;
+        $this->tagRepository = $tagRepository;
+    }
+
+    /**
+     * @throws CouldNotTagOrderException
+     */
+    public function addTagToSubscriptionOrder(SubscriptionEntity $entity, Context $context): void
+    {
+        $orderId = $entity->getOrderId();
+        $subscriptionTag = SubscriptionTag::create();
+
+        // Fetch the order
+        $criteria = new Criteria([$orderId]);
+        $criteria->addAssociation('tags');
+
+        /** @var null|OrderEntity $order */
+        $order = $this->orderRepository->search($criteria, $context)->get($orderId);
+
+        if (!$order instanceof OrderEntity) {
+            throw CouldNotTagOrderException::forSubscription(sprintf('Order with ID "%s" not found', $orderId));
+        }
+
+        // Fetch or create the tag
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('id', $subscriptionTag->getId()));
+        /** @var null|TagEntity $tag */
+        $tag = $this->tagRepository->search($criteria, $context)->first();
+
+        if (!$tag instanceof TagEntity) {
+            throw CouldNotTagOrderException::forSubscription(sprintf('Tag with name "%s" and ID "%s" not found', $subscriptionTag->getName(), $subscriptionTag->getId()));
+        }
+
+        $orderTags = $order->getTags();
+
+        if (!$orderTags instanceof TagCollection) {
+            throw CouldNotTagOrderException::forSubscription(sprintf('Order with ID "%s" does not provide its tag collection', $entity->getOrderId()));
+        }
+
+        $orderTags->add($tag);
+
+        $this->orderRepository->update([
+            [
+                'id' => $orderId,
+                'tags' => array_map(
+                    Closure::fromCallable([$this, 'serializeTag']),
+                    $orderTags->getElements()
+                ),
+            ],
+        ], $context);
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    private function serializeTag(TagEntity $tag): array
+    {
+        return ['id' => $tag->getId(),];
+    }
+}
