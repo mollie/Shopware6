96626b928bab1df4e4fb1a8afa2e856fc7d56cc6
Marvin Muxfeld <marvin.muxfeld@dasistweb.de>
2024-10-01 12:32:38 +0200

    PISHPS-306: cypress fix

--- src/Service/Refund/Exceptions/CreditNoteException.php
+++ src/Service/Refund/Exceptions/CreditNoteException.php
@@ -14,9 +14,9 @@ class CreditNoteException extends \Exception
         parent::__construct($message, $code);
     }
 
-    public static function forAddingLineItems(string $message): CreditNoteException
+    public static function forAddingLineItems(string $message, int $code = self::CODE_ADDING_CREDIT_NOTE_LINE_ITEMS): CreditNoteException
     {
-        return new self($message, self::CODE_ADDING_CREDIT_NOTE_LINE_ITEMS);
+        return new self($message, $code);
     }
 
     public static function forRemovingLineItems(string $message, int $code = self::CODE_REMOVING_CREDIT_NOTE_LINE_ITEMS): CreditNoteException
