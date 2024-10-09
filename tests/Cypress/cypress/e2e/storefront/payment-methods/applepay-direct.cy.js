3008fc03a6c4a5496220fe6a00e30fcd647caac5
Vitalij Mik <vitalij.mik@dasistweb.de>
2024-10-09 09:57:29 +0200

    NTR: revert cypress

--- tests/Cypress/cypress/e2e/storefront/payment-methods/applepay-direct.cy.js
+++ tests/Cypress/cypress/e2e/storefront/payment-methods/applepay-direct.cy.js
@@ -280,7 +280,7 @@ describe('Apple Pay Direct - UI Tests', () => {
                 topMenu.clickOnSecondCategory();
                 listing.clickOnFirstProduct();
 
-                repoPDP.getApplePayDirectButton().should('not.have.class', 'd-none');
+                repoPDP.getApplePayDirectButton().should('have.class', 'd-none');
             })
 
         })
@@ -304,7 +304,7 @@ describe('Apple Pay Direct - UI Tests', () => {
                 cy.visit('/');
                 topMenu.clickOnSecondCategory();
 
-                repoListing.getApplePayDirectButton().should('not.have.class', 'd-none');
+                repoListing.getApplePayDirectButton().should('have.class', 'd-none');
             })
         })
 
@@ -332,7 +332,7 @@ describe('Apple Pay Direct - UI Tests', () => {
                 listing.clickOnFirstProduct();
                 pdp.addToCart(1);
 
-                repoOffcanvas.getApplePayDirectButton().should('not.have.class', 'd-none');
+                repoOffcanvas.getApplePayDirectButton().should('have.class', 'd-none');
             })
 
         })
@@ -364,7 +364,7 @@ describe('Apple Pay Direct - UI Tests', () => {
 
                 checkout.goToCartInOffCanvas();
 
-                repoCart.getApplePayDirectButton().should('not.have.class', 'd-none');
+                repoCart.getApplePayDirectButton().should('have.class', 'd-none');
             })
 
         })
