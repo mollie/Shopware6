3f36a449f10e5f31ef6b330ff969b154804186bf
Vitalij Mik <BlackScorp@users.noreply.github.com>
2024-12-16 15:08:34 +0100

    NTR: prepare new version (#918)
    Co-authored-by: Vitalij Mik <vitalij.mik@dasistweb.de>

--- src/MolliePayments.php
+++ src/MolliePayments.php
@@ -20,7 +20,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class MolliePayments extends Plugin
 {
-    const PLUGIN_VERSION = '4.12.1';
+    const PLUGIN_VERSION = '4.13.0';
 
 
     /**
