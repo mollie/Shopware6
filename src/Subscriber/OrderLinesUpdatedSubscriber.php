d3fcc72e0ffb7589404cd9a6a5991f012715731c
Vitalij Mik <BlackScorp@users.noreply.github.com>
2024-12-16 14:11:19 +0100

    NTR: PISHPS-393: send delivery on cancel item (#916)
    Co-authored-by: Vitalij Mik <vitalij.mik@dasistweb.de>

---
+++ src/Subscriber/OrderLinesUpdatedSubscriber.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace Kiener\MolliePayments\Subscriber;
+
+use Kiener\MolliePayments\Event\OrderLinesUpdatedEvent;
+use Mollie\Api\Resources\OrderLine;
+use Mollie\Api\Types\OrderLineType;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class OrderLinesUpdatedSubscriber implements EventSubscriberInterface
+{
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            OrderLinesUpdatedEvent::class => 'onOrderLinesUpdated',
+        ];
+    }
+
+    public function onOrderLinesUpdated(OrderLinesUpdatedEvent $event): void
+    {
+        $mollieOrder = $event->getMollieOrder();
+
+        $shippingOptions = [];
+
+        $mollieLines = $mollieOrder->lines();
+
+        /**
+         * @var OrderLine $line
+         */
+        foreach ($mollieLines as $line) {
+            if ($line->type === OrderLineType::TYPE_SHIPPING_FEE && $line->shippableQuantity > 0) {
+                $shippingOptions[] = [
+                    'id' => $line->id,
+                    'quantity' => $line->quantity,
+                ];
+            }
+        }
+
+        if (count($shippingOptions) === 0) {
+            return;
+        }
+        try {
+            $mollieOrder->createShipment(['lines' => $shippingOptions]);
+        } catch (\Exception $exception) {
+            $this->logger->error("Failed to update shipping costs", ['message' => $exception->getMessage(), 'options' => $shippingOptions]);
+        }
+    }
+}
