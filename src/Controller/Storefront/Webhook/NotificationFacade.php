68d48c3fdf934b9310f7742dc19ecbec1690bbe3
Vitalij Mik <vitalij.mik@dasistweb.de>
2024-12-20 10:55:29 +0100

    NTR: cs fix

--- src/Controller/Storefront/Webhook/NotificationFacade.php
+++ src/Controller/Storefront/Webhook/NotificationFacade.php
@@ -28,7 +28,6 @@ use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionEnti
 use Shopware\Core\Checkout\Order\OrderEntity;
 use Shopware\Core\Checkout\Payment\PaymentMethodEntity;
 use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\ContainsFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
