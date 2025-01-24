84695746f4caf426e0126ff409924c6545dce63b
Vitalij Mik <vitalij.mik@dasistweb.de>
2025-01-24 12:25:44 +0100

    NTR: Cast string to integer

--- shopware/Component/Settings/Struct/LoggerSettings.php
+++ shopware/Component/Settings/Struct/LoggerSettings.php
@@ -28,7 +28,7 @@ final class LoggerSettings extends Struct
         $logFileDays = $settings[self::KEY_LOG_FILE_DAYS] ?? 0;
         $debugMode = $settings[self::KEY_DEBUG_MODE] ?? false;
 
-        return new LoggerSettings($debugMode, $logFileDays);
+        return new LoggerSettings($debugMode, (int)$logFileDays);
     }
 
     public function isDebugMode(): bool
