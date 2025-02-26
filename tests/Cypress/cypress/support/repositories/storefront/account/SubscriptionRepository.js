be1b72ba1bd5fc8f7eab4cf9b9e08521de226609
Christian <boxblinkracer@gmx.net>
2025-02-26 13:07:40 +0100

    NTR: finish a11y tests for storefront subscriptions in account (#962)

--- tests/Cypress/cypress/support/repositories/storefront/account/SubscriptionRepository.js
+++ tests/Cypress/cypress/support/repositories/storefront/account/SubscriptionRepository.js
@@ -17,4 +17,39 @@ export default class SubscriptionRepository {
     getSubscriptionViewButton(index) {
         return cy.get(':nth-child(' + (index + 1) + ') > .order-wrapper > .order-item-header > .flex-wrap > .subscription-toggle-button-wrapper > .btn');
     }
+
+    /**
+     *
+     * @param index
+     * @returns {Cypress.Chainable<JQuery<HTMLElement>>}
+     */
+    getSubscriptionEditBillingAddressButton(index) {
+        return cy.get('[data-test="btn-subscription-edit-billing-address"]').eq(index);
+    }
+
+    /**
+     *
+     * @param index
+     * @returns {Cypress.Chainable<JQuery<HTMLElement>>}
+     */
+    getSubscriptionEditShippingAddressModal(index) {
+        return cy.get('[data-test="btn-subscription-edit-shipping-address"]').eq(index);
+    }
+
+    /**
+     *
+     * @returns {Cypress.Chainable<JQuery<HTMLElement>>}
+     */
+    getSubscriptionEditBillingAddressModalSaveButton(index) {
+        return cy.get('[data-test="btn-subscription-edit-billing-address-save"]').eq(index);
+    }
+
+    /**
+     *
+     * @returns {Cypress.Chainable<JQuery<HTMLElement>>}
+     */
+    getSubscriptionEditShippingAddressModalSaveButton(index) {
+        return cy.get('[data-test="btn-subscription-edit-shipping-address-save"]').eq(index);
+    }
+
 }
