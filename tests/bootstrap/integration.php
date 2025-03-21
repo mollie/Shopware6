48b16905808535a14b53f17236fedf211a084461
Vitalij Mik <vitalij.mik@dasistweb.de>
2025-03-21 11:43:53 +0100

    NTR: update bootstrapper

--- tests/bootstrap/integration.php
+++ tests/bootstrap/integration.php
@@ -3,18 +3,10 @@
 use Shopware\Core\TestBootstrapper;
 
 $projectDir = realpath(__DIR__ . '/../../../../../');
+$pluginDir = realpath(__DIR__ . '/../../');
 
-$loader = (new TestBootstrapper())
+(new TestBootstrapper())
     ->setProjectDir($projectDir)
     ->setPlatformEmbedded(false)
-    ->addCallingPlugin()
-    ->addActivePlugins('MolliePayments')
-    ->setForceInstallPlugins(true)
-    ->bootstrap()
-    ->getClassLoader();
-
-
-$loader->addPsr4('Kiener\\MolliePayments\\', __DIR__ . '/../../src/');
-$loader->addPsr4('Mollie\\Shopware\\', __DIR__ . '/../../shopware/');
-$loader->addPsr4("Shopware\\Core\\", __DIR__ . '/../../polyfill/Shopware/Core/');
-$loader->addPsr4('Mollie\\Integration\\', __DIR__ . '/../Integration/');
+    ->addCallingPlugin($pluginDir.'/composer.json')
+    ->bootstrap();
