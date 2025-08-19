8e09ecd7b80d1056555b2048888cd5698bde7678
vitalijmik <vitalij.mik@dasistweb.de>
2025-08-19 12:56:42 +0200

    NTR: pr fix

--- shopware/Component/StatusUpdate/UpdateStatusScheduledTask.php
+++ shopware/Component/StatusUpdate/UpdateStatusScheduledTask.php
@@ -7,6 +7,8 @@ use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTask;
 
 final class UpdateStatusScheduledTask extends ScheduledTask
 {
+    protected const MINUTELY = 60;
+
     public static function getTaskName(): string
     {
         return 'mollie.status.update';
