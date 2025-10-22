99fdd7abfb3d6d43a8a8072431e4b1e5a60fd7db
vitalijmik <vitalij.mik@dasistweb.de>
2025-10-22 14:49:03 +0200

    NTR: try to fix pipeline

--- tests/Behat/Context/BootstrapContext.php
+++ tests/Behat/Context/BootstrapContext.php
@@ -7,11 +7,12 @@ use Behat\Hook\BeforeSuite;
 
 final class BootstrapContext implements Context
 {
-    #[BeforeSuite]
-    public static function bootstrap()
+    /**
+     * @BeforeSuite
+     */
+    public static function bootstrap(): void
     {
-
-        require_once __DIR__.'/../../bootstrap.php'; // or just inline your bootstrapping here, depending what you need
+        require_once __DIR__ . '/../../bootstrap.php'; // or just inline your bootstrapping here, depending what you need
     }
 
 }
