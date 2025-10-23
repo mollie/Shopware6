f44ae3e5b2b19b782e8f18d97171c8024f00f7f7
vitalijmik <vitalij.mik@dasistweb.de>
2025-10-23 15:58:51 +0200

    NTR: try to update mollie api

---
+++ shopware/Component/Payment/Mollie/RequestFactory.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Mollie\Shopware\Component\Payment\Mollie;
+
+use Mollie\Api\Http\Data\Money;
+use Mollie\Api\Http\Requests\CreatePaymentRequest;
+use Shopware\Core\Checkout\Order\OrderEntity;
+
+final class RequestFactory implements RequestFactoryInterface
+{
+    public function createPayment(OrderEntity $order): CreatePaymentRequest
+    {
+
+
+        return new CreatePaymentRequest($order->getOrderNumber(), new Money($order->getCurrency()->getIsoCode(), (string)$order->getAmountNet()));
+    }
+}
