580c1cde0ad09723a799b7e792bd94c9dd361144
vitalijmik <vitalij.mik@dasistweb.de>
2025-10-23 15:58:51 +0200

    NTR: try to update mollie api

--- shopware/Component/Settings/AbstractSettingsService.php
+++ shopware/Component/Settings/AbstractSettingsService.php
@@ -3,11 +3,17 @@ declare(strict_types=1);
 
 namespace Mollie\Shopware\Component\Settings;
 
+use Mollie\Shopware\Component\Settings\Struct\ApiSettings;
 use Mollie\Shopware\Component\Settings\Struct\LoggerSettings;
+use Mollie\shopware\Component\Settings\Struct\PaymentSettings;
 
 abstract class AbstractSettingsService
 {
     abstract public function getDecorated(): AbstractSettingsService;
 
     abstract public function getLoggerSettings(?string $salesChannelId = null): LoggerSettings;
+
+    abstract public function getApiSettings(?string $salesChannelId = null): ApiSettings;
+
+    abstract public function getPaymentSettings(?string $salesChannelId = null): PaymentSettings;
 }
