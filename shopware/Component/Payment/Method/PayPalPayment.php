064ab878f2143425b260c6b36f900e043853c159
vitalijmik <vitalij.mik@dasistweb.de>
2025-10-23 15:01:10 +0200

    NTR: WIP: Payments API

---
+++ shopware/Component/Payment/Method/PayPalPayment.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+namespace Mollie\Shopware\Component\Payment\Method;
+
+
+use Mollie\Shopware\Component\Payment\Handler\CompatibilityPaymentHandler;
+
+final class PayPalPayment extends CompatibilityPaymentHandler
+{
+
+}
+
