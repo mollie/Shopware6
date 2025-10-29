1acea40445bcee890008638737044bb322a6f0d6
vitalijmik <vitalij.mik@dasistweb.de>
2025-10-29 12:59:27 +0100

    NTR: add manual capture for klarna

---
+++ shopware/Component/Mollie/CaptureMode.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Mollie\Shopware\Component\Mollie;
+
+final class CaptureMode extends AbstractEnum
+{
+    public const MANUAL = 'manual';
+    public const AUTOMATIC = 'automatic';
+
+    protected function getPossibleValues(): array
+    {
+        return [
+            self::MANUAL,
+            self::AUTOMATIC,
+        ];
+    }
+}
--- shopware/Entity/OrderTransaction/OrderTransaction.php
+++ shopware/Entity/OrderTransaction/OrderTransaction.php
@@ -7,9 +7,16 @@ use Shopware\Core\Framework\Struct\ArrayStruct;
 
 final class OrderTransaction extends ArrayStruct
 {
-    public function __construct(private string $paymentId, private string $finalizeUrl, private int $countPayments = 1)
+    public const PAYMENTS_API_FLAG = 'paymentsApi';
+
+    public function __construct(private string $paymentId, private string $finalizeUrl, private int $countPayments = 1, private bool $paymentsApi = true)
     {
-        parent::__construct(['paymentId' => $this->paymentId, 'finalizeUrl' => $this->finalizeUrl, 'countPayments' => $this->countPayments], 'mollie_order_transaction');
+        parent::__construct([
+            self::PAYMENTS_API_FLAG => $this->paymentsApi,
+            'paymentId' => $this->paymentId,
+            'finalizeUrl' => $this->finalizeUrl,
+            'countPayments' => $this->countPayments
+        ], 'mollie_order_transaction');
     }
 
     public function getPaymentId(): string
