05048ff26da1b8d5be3192f4f249ca5edc2fc04c
vitalijmik <vitalij.mik@dasistweb.de>
2025-10-30 10:54:17 +0100

    NTR: refactor webhook

--- shopware/Component/Payment/Route/WebhookRouteResponse.php
+++ shopware/Component/Payment/Route/WebhookRouteResponse.php
@@ -3,8 +3,17 @@ declare(strict_types=1);
 
 namespace Mollie\Shopware\Component\Payment\Route;
 
+use Shopware\Core\Framework\Struct\ArrayStruct;
 use Shopware\Core\System\SalesChannel\StoreApiResponse;
 
 final class WebhookRouteResponse extends StoreApiResponse
 {
+    public function __construct()
+    {
+        parent::__construct(new ArrayStruct(
+            [
+                'status' => 'success',
+            ], 'webhook.route.success'
+        ));
+    }
 }
