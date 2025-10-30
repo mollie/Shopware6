b1e8177b61e6a328ec131368165cc23a9a6d37c7
vitalijmik <vitalij.mik@dasistweb.de>
2025-10-30 10:54:17 +0100

    NTR: refactor webhook

--- shopware/Component/Payment/Route/AbstractWebhookRoute.php
+++ shopware/Component/Payment/Route/AbstractWebhookRoute.php
@@ -3,11 +3,11 @@ declare(strict_types=1);
 
 namespace Mollie\Shopware\Component\Payment\Route;
 
-use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Core\Framework\Context;
 
 abstract class AbstractWebhookRoute
 {
     abstract public function getDecorated(): self;
 
-    abstract public function notify(string $transactionId, SalesChannelContext $salesChannelContext): WebhookRouteResponse;
+    abstract public function notify(string $transactionId, Context $context): WebhookRouteResponse;
 }
