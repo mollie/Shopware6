caa3552ecd58e1d53c3e4e7f4c7f7a6a460c6c56
vitalijmik <vitalij.mik@dasistweb.de>
2025-10-31 14:37:07 +0100

    NTR: update version

--- src/MolliePayments.php
+++ src/MolliePayments.php
@@ -18,7 +18,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class MolliePayments extends Plugin
 {
-    public const PLUGIN_VERSION = '4.24.0';
+    public const PLUGIN_VERSION = '5.0.0-beta3';
 
     /**
      * @throws \Exception
@@ -73,9 +73,9 @@ class MolliePayments extends Plugin
     {
         parent::activate($context);
 
-        $this->runDbMigrations($context->getMigrationCollection());
-
         $this->preparePlugin($context->getContext());
+
+        $this->runDbMigrations($context->getMigrationCollection());
     }
 
     public function boot(): void
