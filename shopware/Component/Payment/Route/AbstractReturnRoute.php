b70662b78fde7e51e75eabed407d012d2c68a2b8
Vitalij Mik <cccpmik@gmail.com>
2025-11-09 21:10:41 +0100

    updated webhooks

--- shopware/Component/Payment/Route/AbstractReturnRoute.php
+++ shopware/Component/Payment/Route/AbstractReturnRoute.php
@@ -3,12 +3,12 @@ declare(strict_types=1);
 
 namespace Mollie\Shopware\Component\Payment\Route;
 
-use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Core\Framework\Context;
 use Symfony\Component\HttpFoundation\Request;
 
 abstract class AbstractReturnRoute
 {
     abstract public function getDecorated(): self;
 
-    abstract public function return(Request $request, SalesChannelContext $context): ReturnRouteResponse;
+    abstract public function return(Request $request, Context $context): ReturnRouteResponse;
 }
