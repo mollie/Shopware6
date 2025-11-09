b00c602caa354aabc7a407e948bde738e6417459
Vitalij Mik <cccpmik@gmail.com>
2025-11-09 15:50:41 +0100

    added settings struct tests

---
+++ tests/Unit/Settings/LoggerSettingsTest.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Mollie\Unit\Settings;
+
+use Mollie\Shopware\Component\Settings\Struct\LoggerSettings;
+use PHPUnit\Framework\TestCase;
+
+final class LoggerSettingsTest extends TestCase
+{
+    public function testCanCreateApiSettingsFromArray(): void
+    {
+        $data = [
+            LoggerSettings::KEY_DEBUG_MODE => false,
+            LoggerSettings::KEY_LOG_FILE_DAYS => 5,
+        ];
+        $settings = LoggerSettings::createFromShopwareArray($data);
+
+        $this->assertFalse($settings->isDebugMode());
+        $this->assertSame(5, $settings->getLogFileDays());
+    }
+}
