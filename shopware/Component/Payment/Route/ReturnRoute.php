6338c632318474385d44512af0f82692728d33a6
Vitalij Mik <cccpmik@gmail.com>
2025-11-09 21:10:41 +0100

    updated webhooks

--- shopware/Component/Payment/Route/ReturnRoute.php
+++ shopware/Component/Payment/Route/ReturnRoute.php
@@ -6,7 +6,6 @@ namespace Mollie\Shopware\Component\Payment\Route;
 use Mollie\Shopware\Component\Mollie\Gateway\MollieGatewayInterface;
 use Psr\Log\LoggerInterface;
 use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
-use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Symfony\Component\HttpFoundation\Request;
 
 final class ReturnRoute extends AbstractReturnRoute
@@ -22,10 +21,10 @@ final class ReturnRoute extends AbstractReturnRoute
         throw new DecorationPatternException(self::class);
     }
 
-    public function return(Request $request, SalesChannelContext $context): ReturnRouteResponse
+    public function return(Request $request, Context $context): ReturnRouteResponse
     {
         $transactionId = $request->get('transactionId');
-        $payment = $this->mollieGateway->getPaymentByTransactionId($transactionId, $context->getContext());
+        $payment = $this->mollieGateway->getPaymentByTransactionId($transactionId, $context);
 
         return new ReturnRouteResponse($payment);
     }
