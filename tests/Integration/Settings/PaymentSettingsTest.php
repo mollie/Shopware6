7562ec8a8f454c4de93b860591d096987e65b160
Vitalij Mik <cccpmik@gmail.com>
2025-11-09 16:35:08 +0100

    added more integration tests

--- tests/Integration/Settings/PaymentSettingsTest.php
+++ tests/Integration/Settings/PaymentSettingsTest.php
@@ -4,46 +4,38 @@ declare(strict_types=1);
 namespace Mollie\Integration\Settings;
 
 use Mollie\Shopware\Component\Settings\SettingsService;
-use Mollie\Shopware\Component\Settings\Struct\LoggerSettings;
+use Mollie\Shopware\Component\Settings\Struct\PaymentSettings;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\DevOps\Environment\EnvironmentHelper;
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 
-/**
- * @infection-ignore-all
- */
-final class LoggerSettingsTest extends TestCase
+final class PaymentSettingsTest extends TestCase
 {
     use IntegrationTestBehaviour;
 
-    public function testSettingsCanBeReadFromDatabase(): void
+    public function testSettingsAreLoadedFromDatabase(): void
     {
         /**
          * @var SystemConfigService $systemConfigService
          */
         $systemConfigService = $this->getContainer()->get(SystemConfigService::class);
-        $systemConfigService->set(SettingsService::SYSTEM_CONFIG_DOMAIN . '.' . LoggerSettings::KEY_LOG_FILE_DAYS, 10);
-        $systemConfigService->set(SettingsService::SYSTEM_CONFIG_DOMAIN . '.' . LoggerSettings::KEY_DEBUG_MODE, false);
-
+        $systemConfigService->set(SettingsService::SYSTEM_CONFIG_DOMAIN . '.' . PaymentSettings::KEY_ORDER_NUMBER_FORMAT, 'test_{ordernumber}_{customernumber}');
         $devMode = (bool) EnvironmentHelper::getVariable('MOLLIE_DEV_MODE', false);
         $cypressMode = (bool) EnvironmentHelper::getVariable('MOLLIE_CYPRESS_MODE', false);
         $settingsService = new SettingsService($this->getContainer(), $devMode, $cypressMode);
-
-        $actualSettings = $settingsService->getLoggerSettings();
-
-        $this->assertSame(10, $actualSettings->getLogFileDays());
-        $this->assertFalse($actualSettings->isDebugMode());
+        $paymentSettings = $settingsService->getPaymentSettings();
+        $this->assertSame('test_{ordernumber}_{customernumber}', $paymentSettings->getOrderNumberFormat());
     }
 
     public function testSettingsAreCachedPerSalesChannel(): void
     {
         $settingsService = $this->getContainer()->get(SettingsService::class);
 
-        $expectedSettings = $settingsService->getLoggerSettings();
-        $actualSettings = $settingsService->getLoggerSettings();
-        $differentSalesChannelSettings = $settingsService->getLoggerSettings(Uuid::randomHex());
+        $expectedSettings = $settingsService->getPaymentSettings();
+        $actualSettings = $settingsService->getPaymentSettings();
+        $differentSalesChannelSettings = $settingsService->getPaymentSettings(Uuid::randomHex());
 
         $this->assertSame($expectedSettings, $actualSettings);
         $this->assertNotSame($actualSettings, $differentSalesChannelSettings);
