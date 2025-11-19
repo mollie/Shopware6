512ab80fbee6c787d062ca55b738515313035ffc
Vitalij Mik <BlackScorp@users.noreply.github.com>
2025-11-19 11:28:30 +0100

    NTR: fix cypress tests (#1134)
    * NTR: fix cypress tests
    
    * NTR: fix cypress tests
    
    ---------
    
    Co-authored-by: vitalijmik <vitalij.mik@dasistweb.de>

--- tests/Cypress/cypress/support/repositories/admin/orders/OrdersListRepository.js
+++ tests/Cypress/cypress/support/repositories/admin/orders/OrdersListRepository.js
@@ -27,7 +27,7 @@ export default class OrdersListRepository {
      * @returns {string}
      */
     getLatestPaymentStatusLabelSelector() {
-        return ".sw-data-grid__row--0 > .sw-data-grid__cell--transactions-last\\(\\)-stateMachineState-name > .sw-data-grid__cell-content";
+        return '.sw-data-grid__row--0 > [class*="ansaction-stateMachineState-name"] > .sw-data-grid__cell-content';
     }
 
 }
