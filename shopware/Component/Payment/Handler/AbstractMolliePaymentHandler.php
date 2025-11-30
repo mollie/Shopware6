50b6e0f00cf8941b844f9f4bc12e5e242d171a61
Vitalij Mik <cccpmik@gmail.com>
2025-11-30 22:05:57 +0100

    added easy payment methods

--- shopware/Component/Payment/Handler/AbstractMolliePaymentHandler.php
+++ shopware/Component/Payment/Handler/AbstractMolliePaymentHandler.php
@@ -5,32 +5,19 @@ namespace Mollie\Shopware\Component\Payment\Handler;
 
 use Mollie\Shopware\Component\Mollie\CreatePayment;
 use Mollie\Shopware\Component\Mollie\PaymentMethod;
-use Mollie\Shopware\Component\Payment\Action\Finalize;
-use Mollie\Shopware\Component\Payment\Action\Pay;
-use Mollie\Shopware\Component\Transaction\TransactionConverterInterface;
-use Psr\Log\LoggerInterface;
 use Shopware\Core\Checkout\Order\OrderEntity;
 use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\AbstractPaymentHandler;
 use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\PaymentHandlerType;
 use Shopware\Core\Checkout\Payment\Cart\PaymentTransactionStruct;
-use Shopware\Core\Checkout\Payment\PaymentException;
 use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\HttpException;
 use Shopware\Core\Framework\Struct\Struct;
-use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
-use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 abstract class AbstractMolliePaymentHandler extends AbstractPaymentHandler
 {
-    protected PaymentMethod $method;
-
-    public function __construct(private Pay $pay,
-        private Finalize $finalize,
-        private TransactionConverterInterface $transactionConverter,
-        private LoggerInterface $logger,
-    ) {
+    public function __construct(private MolliePaymentFacade $paymentFacade)
+    {
     }
 
     public function supports(PaymentHandlerType $type, string $paymentMethodId, Context $context): bool
@@ -40,43 +27,12 @@ abstract class AbstractMolliePaymentHandler extends AbstractPaymentHandler
 
     public function pay(Request $request, PaymentTransactionStruct $transaction, Context $context, ?Struct $validateStruct): ?RedirectResponse
     {
-        /** @var SalesChannelContext $salesChannelContext */
-        $salesChannelContext = $request->get('sw-sales-channel-context');
-        $dataBag = new RequestDataBag($request->request->all());
-        $shopwareTransaction = $transaction;
-
-        try {
-            $transaction = $this->transactionConverter->convert($shopwareTransaction, $salesChannelContext->getContext());
-
-            return $this->pay->execute($this, $transaction, $dataBag, $salesChannelContext);
-        } catch (\Throwable $exception) {
-            $this->logger->error('Mollie Pay Process Failed', [
-                'error' => $exception->getMessage(),
-                'paymentMethod' => $this->getPaymentMethod()->value
-            ]);
-            throw PaymentException::asyncProcessInterrupted($shopwareTransaction->getOrderTransactionId(), $exception->getMessage(), $exception);
-        }
+        return $this->paymentFacade->pay($this, $request, $transaction, $context, $validateStruct);
     }
 
     public function finalize(Request $request, PaymentTransactionStruct $transaction, Context $context): void
     {
-        $shopwareTransaction = $transaction;
-        try {
-            $transaction = $this->transactionConverter->convert($shopwareTransaction, $context);
-            $this->finalize->execute($transaction, $context);
-        } catch (HttpException $exception) {
-            $this->logger->error('Payment is aborted or failed', [
-                'error' => $exception->getMessage(),
-                'paymentMethod' => $this->getPaymentMethod()->value
-            ]);
-            throw $exception;
-        } catch (\Throwable $exception) {
-            $this->logger->error('Payment failed unexpected', [
-                'error' => $exception->getMessage(),
-                'paymentMethod' => $this->getPaymentMethod()->value
-            ]);
-            throw PaymentException::asyncFinalizeInterrupted($shopwareTransaction->getOrderTransactionId(), $exception->getMessage(), $exception);
-        }
+        $this->paymentFacade->finalize($this, $request, $transaction, $context);
     }
 
     public function applyPaymentSpecificParameters(CreatePayment $payment, OrderEntity $orderEntity): CreatePayment
@@ -84,8 +40,5 @@ abstract class AbstractMolliePaymentHandler extends AbstractPaymentHandler
         return $payment;
     }
 
-    public function getPaymentMethod(): PaymentMethod
-    {
-        return $this->method;
-    }
+    abstract public function getPaymentMethod(): PaymentMethod;
 }
