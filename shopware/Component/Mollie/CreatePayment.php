949ff387299e46c456d2ac5a1efffd8f15a4411e
Vitalij Mik <cccpmik@gmail.com>
2025-11-30 12:12:59 +0100

    mollie gateway coverage is 100%

--- shopware/Component/Mollie/CreatePayment.php
+++ shopware/Component/Mollie/CreatePayment.php
@@ -23,6 +23,7 @@ final class CreatePayment implements \JsonSerializable
     private string $sequenceType;
 
     private ?string $cardToken = null;
+    private string $shopwareOrderNumber;
 
     public function __construct(string $description, string $redirectUrl, Money $amount)
     {
@@ -157,5 +158,15 @@ final class CreatePayment implements \JsonSerializable
     public function toArray(): array
     {
         return json_decode((string) json_encode($this), true);
+    }
+
+    public function setShopwareOrderNumber(string $orderNumber): void
+    {
+        $this->shopwareOrderNumber = $orderNumber;
+    }
+
+    public function getShopwareOrderNumber(): string
+    {
+        return $this->shopwareOrderNumber;
     }
 }
