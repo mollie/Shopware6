432b5a95dcfe1cd03ac7a49d4781b2ccacc198d8
vitalijmik <vitalij.mik@dasistweb.de>
2025-12-05 15:39:26 +0100

    NTR: refactor voucher

--- shopware/Component/Payment/RemovePaymentMethodRoute.php
+++ shopware/Component/Payment/RemovePaymentMethodRoute.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace Mollie\Shopware\Component\Payment;
 
-use Mollie\Shopware\Component\Payment\MethodRemover\PaymentMethodRemoverInterface;
+use Mollie\Shopware\Component\Payment\MethodRemover\AbstractPaymentRemover;
 use Shopware\Core\Checkout\Payment\SalesChannel\AbstractPaymentMethodRoute;
 use Shopware\Core\Checkout\Payment\SalesChannel\PaymentMethodRoute;
 use Shopware\Core\Checkout\Payment\SalesChannel\PaymentMethodRouteResponse;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Storefront\Controller\AccountOrderController;
+use Shopware\Storefront\Controller\CheckoutController;
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,7 +19,7 @@ use Symfony\Component\HttpFoundation\Request;
 final class RemovePaymentMethodRoute extends AbstractPaymentMethodRoute
 {
     /**
-     * @param PaymentMethodRemoverInterface[] $paymentMethodRemovers
+     * @param AbstractPaymentRemover[] $paymentMethodRemovers
      */
     public function __construct(private AbstractPaymentMethodRoute $decorated,
         #[AutowireIterator('mollie.method.remover')]
@@ -34,15 +36,53 @@ final class RemovePaymentMethodRoute extends AbstractPaymentMethodRoute
     {
         $response = $this->decorated->load($request, $context, $criteria);
 
+        if ($this->shouldRemove($request) === false) {
+            return $response;
+        }
+
+        $orderId = $request->get('orderId','');
+
         $paymentMethods = $response->getPaymentMethods();
 
-        /** @var PaymentMethodRemoverInterface $paymentMethodRemover */
+        /** @var AbstractPaymentRemover $paymentMethodRemover */
         foreach ($this->paymentMethodRemovers as $paymentMethodRemover) {
-            $paymentMethods = $paymentMethodRemover->remove($paymentMethods, $request, $context);
+            $paymentMethods = $paymentMethodRemover->remove($paymentMethods, $orderId, $context);
         }
         $response = $response->getObject();
         $response->assign(['entities' => $paymentMethods, 'elements' => $paymentMethods, 'total' => $paymentMethods->count()]);
 
         return new PaymentMethodRouteResponse($response);
+    }
+
+    private function getControllerClass(Request $request): ?string
+    {
+        $controller = $request->attributes->get('_controller');
+        if ($controller === null) {
+            return null;
+        }
+        $controllerParts = explode('::', $controller);
+
+        $controllerClass = $controllerParts[0] ?? null;
+        if ($controllerClass === null) {
+            return null;
+        }
+
+        return $controllerClass;
+    }
+
+    private function shouldRemove(Request $request): bool
+    {
+        $controllerClass = $this->getControllerClass($request);
+        if ($controllerClass === null) {
+            return false;
+        }
+
+        return in_array(
+            $controllerClass,
+            [
+                CheckoutController::class,
+                AccountOrderController::class,
+            ]
+        );
     }
 }
