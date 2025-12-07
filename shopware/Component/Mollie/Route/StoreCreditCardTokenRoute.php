bdcf7b5cd9897ba29b62a4cbfecfaf446bfdbc2d
Vitalij Mik <cccpmik@gmail.com>
2025-12-07 13:07:37 +0100

    added mandate route and refactored routes

---
+++ shopware/Component/Mollie/Route/StoreCreditCardTokenRoute.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Mollie\Shopware\Component\Mollie\Route;
+
+use Psr\Log\LoggerInterface;
+use Shopware\Core\Framework\Context;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(defaults: ['_routeScope' => ['store-api']])]
+final class StoreCreditCardTokenRoute
+{
+    public function __construct(
+        #[Autowire(service: 'monolog.logger.mollie')]
+        private LoggerInterface $logger,
+    ) {
+    }
+
+    #[Route(path: '/store-api/mollie/creditcard/store-token/{customerId}/{cardToken}', name: 'store-api.mollie.creditcard.store-token', methods: ['POST'])]
+    public function store(string $customerId,string $cardToken, Context $context): StoreCreditCardTokenResponse
+    {
+        $this->logger->warning('Deprecated route was called, credit card tokens are not stored anymore, please provide "creditCardToken" in request body for payment');
+
+        return new StoreCreditCardTokenResponse();
+    }
+}
--- shopware/Component/Payment/Route/AbstractReturnRoute.php
+++ shopware/Component/Payment/Route/AbstractReturnRoute.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 namespace Mollie\Shopware\Component\Payment\Route;
 
 use Shopware\Core\Framework\Context;
-use Symfony\Component\HttpFoundation\Request;
 
 abstract class AbstractReturnRoute
 {
     abstract public function getDecorated(): self;
 
-    abstract public function return(Request $request, Context $context): ReturnRouteResponse;
+    abstract public function return(string $transactionId, Context $context): ReturnRouteResponse;
 }
