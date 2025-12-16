501ec32d546d5d08e6ace78d12a6a019bc91e0bb
Vitalij Mik <cccpmik@gmail.com>
2025-12-16 19:25:40 +0100

    disable crlf

--- tests/Unit/Payment/Fake/FakeBankTransferAwarePaymentHandler.php
+++ tests/Unit/Payment/Fake/FakeBankTransferAwarePaymentHandler.php
@@ -4,20 +4,13 @@ declare(strict_types=1);
 namespace Mollie\Shopware\Unit\Payment\Fake;
 
 use Mollie\Shopware\Component\Mollie\PaymentMethod;
-use Mollie\Shopware\Component\Payment\Action\Finalize;
-use Mollie\Shopware\Component\Payment\Action\Pay;
 use Mollie\Shopware\Component\Payment\Handler\AbstractMolliePaymentHandler;
 use Mollie\Shopware\Component\Payment\Handler\BankTransferAwareInterface;
-use Psr\Log\LoggerInterface;
 
 final class FakeBankTransferAwarePaymentHandler extends AbstractMolliePaymentHandler implements BankTransferAwareInterface
 {
     public function __construct(
-        Pay $pay,
-        Finalize $finalize,
-        LoggerInterface $logger,
     ) {
-        parent::__construct($pay, $finalize, $logger);
     }
 
     public function getPaymentMethod(): PaymentMethod
