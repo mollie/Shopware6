92f85788636b1559534a58d971b779fcbd07df43
vitalijmik <vitalij.mik@dasistweb.de>
2025-12-18 08:11:59 +0100

    NTR: csfix

--- shopware/Component/Payment/Route/PaymentException.php
+++ shopware/Component/Payment/Route/PaymentException.php
@@ -12,6 +12,7 @@ final class PaymentException extends HttpException
     public const TRANSACTION_WITHOUT_PAYMENT_METHOD = 'TRANSACTION_WITHOUT_PAYMENT_METHOD';
     public const TRANSACTION_WITHOUT_MOLLIE_PAYMENT = 'TRANSACTION_WITHOUT_MOLLIE_PAYMENT';
     public const NEW_PAYMENT_METHOD_NOT_FOUND = 'NEW_PAYMENT_METHOD_NOT_FOUND';
+    public const PAYMENT_WITHOUT_METHOD = 'PAYMENT_WITHOUT_METHOD';
 
     public static function transactionWithoutOrder(string $transactionId): self
     {
@@ -54,6 +55,18 @@ final class PaymentException extends HttpException
             'Transaction {transactionId} has {paymentMethod} in mollie API, but this method is not active in shop',[
                 'transactionId' => $transactionId,
                 'paymentMethod' => $paymentMethod,
+            ]
+        );
+    }
+
+    public static function paymentWithoutMethod(string $transactionId, string $paymentId): self
+    {
+        return new self(
+            Response::HTTP_BAD_REQUEST,
+            self::PAYMENT_WITHOUT_METHOD,
+            'Mollie payment {paymentId} is without payment method in transaction {transactionId}',[
+                'transactionId' => $transactionId,
+                'paymentId' => $paymentId,
             ]
         );
     }
