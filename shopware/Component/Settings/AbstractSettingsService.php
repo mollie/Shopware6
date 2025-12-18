9107c341a18bc1673f948f8bfca750ee332e3604
vitalijmik <vitalij.mik@dasistweb.de>
2025-12-18 12:48:23 +0100

    NTR: add order state settings

--- shopware/Component/Settings/AbstractSettingsService.php
+++ shopware/Component/Settings/AbstractSettingsService.php
@@ -9,6 +9,7 @@ use Mollie\Shopware\Component\Settings\Struct\ApplePaySettings;
 use Mollie\Shopware\Component\Settings\Struct\CreditCardSettings;
 use Mollie\Shopware\Component\Settings\Struct\EnvironmentSettings;
 use Mollie\Shopware\Component\Settings\Struct\LoggerSettings;
+use Mollie\Shopware\Component\Settings\Struct\OrderStateSettings;
 use Mollie\Shopware\Component\Settings\Struct\PaymentSettings;
 use Mollie\Shopware\Component\Settings\Struct\PayPalExpressSettings;
 use Mollie\Shopware\Component\Settings\Struct\SubscriptionSettings;
@@ -34,4 +35,6 @@ abstract class AbstractSettingsService
     abstract public function getApplePaySettings(?string $salesChannelId = null): ApplePaySettings;
 
     abstract public function getSubscriptionSettings(?string $salesChannelId = null): SubscriptionSettings;
+
+    abstract public function getOrderStateSettings(?string $salesChannelId = null): OrderStateSettings;
 }
