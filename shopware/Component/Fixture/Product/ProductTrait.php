11397d61cf245c6251b29bb447f55b94b532fd8a
Vitalij Mik <cccpmik@gmail.com>
2026-01-09 13:27:39 +0100

    fixtures refactore

--- shopware/Component/Fixture/Product/ProductTrait.php
+++ shopware/Component/Fixture/Product/ProductTrait.php
@@ -3,14 +3,11 @@ declare(strict_types=1);
 
 namespace Mollie\Shopware\Component\Fixture\Product;
 
-use Shopware\Core\Content\Media\File\FileFetcher;
 use Shopware\Core\Content\Media\MediaEntity;
-use Shopware\Core\Content\Media\MediaService;
 use Shopware\Core\Defaults;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Uuid\Uuid;
 
 trait ProductTrait
@@ -20,13 +17,39 @@ trait ProductTrait
         return Uuid::fromStringToHex($productNumber);
     }
 
-    private function getProductData(string $name, string $productNumber, string $description, string $image, string $category, string $salesChannelId, float $price, ?float $taxRate = null, bool $shippingFree = false, array $customFields = []): array
+    /**
+     * @param array<mixed> $customFields
+     * @param array<mixed> $options
+     *
+     * @return array<mixed>
+     */
+    private function getProductData(string $name, string $productNumber, string $description, string $mediaId, string $category, string $salesChannelId, float $price, float $taxRate = 19.0, int $stock = 10, bool $shippingFree = false, ?string $parentId = null, array $customFields = [], array $options = []): array
     {
         $productId = $this->getProductId($productNumber);
+        $parts = explode('.', (string) $price);
+        $decimals = isset($parts[1]) ? strlen($parts[1]) : 0;
+
+        $netPrice = round($price / (1 + $taxRate / 100), $decimals);
+        $productMediaId = Uuid::fromStringToHex(sprintf('%s-%s', $productId, $mediaId));
+        $variantListingConfig = [];
+        $configuratorSettings = [];
+        if ($parentId === null && count($options) > 0) {
+            $variantListingConfig = [
+                'displayParent' => true
+            ];
+            foreach ($options as $option) {
+                $configuratorSettings[] = [
+                    'id' => Uuid::fromStringToHex(sprintf('%s-%s', $productId, $option['id'])),
+                    'option' => $option,
+                ];
+            }
+        }
+
         return [
             'id' => $productId,
             'name' => $name,
-            'taxId' => $taxRate->getId(),
+            'parentId' => $parentId,
+            'taxId' => Uuid::fromStringToHex('tax-' . $taxRate),
             'productNumber' => $productNumber,
             'description' => $description,
             'visibilities' => [
@@ -41,55 +64,82 @@ trait ProductTrait
                     'id' => Uuid::fromStringToHex('mollie-' . $category),
                 ]
             ],
-            'stock' => 10,
+            'stock' => $stock,
             'price' => [
                 [
                     'currencyId' => Defaults::CURRENCY,
                     'gross' => $price,
-                    'net' => $price,
+                    'net' => $netPrice,
                     'linked' => true,
                 ]
             ],
             'media' => [
                 [
-                    'id' => $coverId,
+                    'id' => $productMediaId,
                     'mediaId' => $mediaId,
                 ]
             ],
-            'coverId' => $coverId,
+            'coverId' => $productMediaId,
             'customFields' => $customFields,
             'shippingFree' => $shippingFree,
+            'options' => $options,
+            'properties' => $options,
+            'variantListingConfig' => $variantListingConfig,
+            'configuratorSettings' => $configuratorSettings,
+            'manufacturer' => [
+                'id' => Uuid::fromStringToHex('mollie-manufacturer'),
+                'name' => 'Mollie',
+            ]
         ];
     }
 
-    private function getMediaId(string $image): string
-    {
-
-
-        $filePath = __DIR__ . '/Assets/' . $image;
-        $fileInfo = new \finfo();
-        $contentType = $fileInfo->file($filePath, FILEINFO_MIME_TYPE);
-        $extension = $fileInfo->file($filePath, FILEINFO_EXTENSION);
-        $fileContent = file_get_contents($filePath);
-        $mediaFile = $this->fileFetcher->fetchBlob($fileContent,$extension,$contentType);
-        $this->fileFetcher->cleanUpTempFile($mediaFile);
-        dump($mediaFile);
-        return '';
-        // $mediaFile = $fileFetcher->fetchBlob(file_get_contents($filePath),$splFileInfo->getExtension(),$splFileInfo->getType());
-    }
-
-    private function getMediaMapping(array $images,Context $context):array
+    /**
+     * @param array<string> $images
+     *
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     *
+     * @return array<string,string>
+     */
+    private function getMediaMapping(array $images, Context $context): array
     {
         $criteria = new Criteria();
-        $criteria->addFilter(new EqualsAnyFilter('fileName', $images));
+        $criteriaImages = array_map(function ($image) {
+            $extension = pathinfo($image, PATHINFO_EXTENSION);
+
+            return str_replace('.' . $extension, '', $image);
+        }, $images);
+
+        $criteria->addFilter(new EqualsAnyFilter('fileName', $criteriaImages));
         $mediaRepository = $this->container->get('media.repository');
         $searchResult = $mediaRepository->search($criteria, $context);
+        $requestedImages = array_flip($images);
 
         $result = [];
         /** @var MediaEntity $media */
         foreach ($searchResult as $media) {
-            $key = sprintf('%s.%s',$media->getFileName(),$media->getFileExtension());
+            $key = sprintf('%s.%s', $media->getFileName(), $media->getFileExtension());
+
             $result[$key] = $media->getId();
+            unset($requestedImages[$key]);
+        }
+
+        foreach ($requestedImages as $image => $number) {
+            $filePath = __DIR__ . '/Assets/' . $image;
+            $fileContent = file_get_contents($filePath);
+            if ($fileContent === false) {
+                continue;
+            }
+
+            $fileInfo = new \finfo();
+            $contentType = (string) $fileInfo->file($filePath, FILEINFO_MIME_TYPE);
+            $extension = (string) $fileInfo->file($filePath, FILEINFO_EXTENSION);
+            $fileName = str_replace('.' . $extension, '', $image);
+
+            $mediaFile = $this->fileFetcher->fetchBlob($fileContent, $extension, $contentType);
+            $mediaId = $this->mediaService->saveMediaFile($mediaFile, $fileName, $context, 'product', null, false);
+            $result[$image] = $mediaId;
+            unset($requestedImages[$image]);
         }
 
         return $result;
