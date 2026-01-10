b008886bb41096ab57ec8c5b15aa5f042d9ea32c
Vitalij Mik <cccpmik@gmail.com>
2026-01-10 21:38:08 +0100

    plugin version is shown only for mollie channel

--- shopware/Component/Logger/Processor/MolliePluginVersionProcessor.php
+++ shopware/Component/Logger/Processor/MolliePluginVersionProcessor.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 namespace Mollie\Shopware\Component\Logger\Processor;
 
 use Kiener\MolliePayments\MolliePayments;
+use Monolog\Attribute\AsMonologProcessor;
 use Monolog\LogRecord;
 use Monolog\Processor\ProcessorInterface;
 
+#[AsMonologProcessor('mollie')]
 class MolliePluginVersionProcessor implements ProcessorInterface
 {
     public function __invoke(LogRecord $record): LogRecord
     {
-        $record->extra['pluginVersion'] = MolliePayments::PLUGIN_VERSION;
+        if ($record->channel === 'mollie') {
+            $record->extra['pluginVersion'] = MolliePayments::PLUGIN_VERSION;
+        }
 
         return $record;
     }
