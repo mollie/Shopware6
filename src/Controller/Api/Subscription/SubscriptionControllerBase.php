12bd4c7000cb669642fb3a93a3c4c33a68e937cc
vitalijmik <vitalij.mik@dasistweb.de>
2026-01-22 11:49:24 +0100

    NTR: csfix

--- src/Controller/Api/Subscription/SubscriptionControllerBase.php
+++ src/Controller/Api/Subscription/SubscriptionControllerBase.php
@@ -27,7 +27,7 @@ class SubscriptionControllerBase extends AbstractController
     private $subscriptionManager;
 
     private MollieApiFactory $mollieApiFactory;
-    
+
     /**
      * @var EntityRepository<CustomerCollection<CustomerEntity>>
      */
