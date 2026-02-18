608d44c3da1c339039c0203fecda87fc05bb1e8a
vitalijmik <vitalij.mik@dasistweb.de>
2026-02-18 15:46:08 +0100

    NTR: added subscription action tests

--- tests/Behat/Context/BootstrapContext.php
+++ tests/Behat/Context/BootstrapContext.php
@@ -1,18 +1,35 @@
 <?php
 declare(strict_types=1);
 
-namespace Mollie\Shopware\Behat;
+namespace Mollie\Shopware\Behat\Context;
 
 use Behat\Behat\Context\Context;
 use Behat\Hook\BeforeSuite;
+use Mollie\Shopware\Behat\Storage;
+use Mollie\Shopware\Component\Settings\SettingsService;
+use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 
 final class BootstrapContext implements Context
 {
+    use IntegrationTestBehaviour;
+
     /**
      * @BeforeSuite
      */
     public static function bootstrap(): void
     {
         require_once __DIR__ . '/../../bootstrap.php'; // or just inline your bootstrapping here, depending what you need
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function clearStorage(): void
+    {
+        /** @var SettingsService $settingsService */
+        $settingsService = $this->getContainer()->get(SettingsService::class);
+        $settingsService->clearCache();
+
+        Storage::clear();
     }
 }
--- tests/Behat/Context/ShopwareContext.php
+++ tests/Behat/Context/ShopwareContext.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Mollie\Shopware\Behat;
+namespace Mollie\Shopware\Behat\Context;
 
 use Behat\Behat\Context\Context;
 use Mollie\Shopware\Integration\Data\SalesChannelTestBehaviour;
