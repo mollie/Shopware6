c255b85cb805aee671c3f26a809f1570d92271ab
vitalijmik <vitalij.mik@dasistweb.de>
2026-02-18 15:46:08 +0100

    NTR: added subscription action tests

---
+++ tests/Integration/Data/SubscriptionTestBehaviour.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Mollie\Shopware\Integration\Data;
+
+use Kiener\MolliePayments\Components\Subscription\DAL\Subscription\SubscriptionCollection;
+use Kiener\MolliePayments\Components\Subscription\DAL\Subscription\SubscriptionEntity;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
+
+trait SubscriptionTestBehaviour
+{
+    use IntegrationTestBehaviour;
+
+    public function getSubscriptionByOrderId(string $orderId, Context $context): SubscriptionEntity
+    {
+        /** @var EntityRepository<SubscriptionCollection<SubscriptionEntity>> $repository */
+        $repository = $this->getContainer()->get('mollie_subscription.repository');
+
+        $criteria = new Criteria();
+
+        $criteria->addFilter(new EqualsFilter('orderId', $orderId));
+
+        return $repository->search($criteria, $context)->first();
+    }
+}
