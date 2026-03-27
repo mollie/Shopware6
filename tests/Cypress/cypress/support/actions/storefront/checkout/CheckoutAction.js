588bc773dbfc5a7f1010b7bf1088ff34c62a5e4a
Vitalij Mik <vitalij.mik@dasistweb.de>
2026-03-27 14:20:22 +0100

    NTR: fix tests

--- tests/Cypress/cypress/support/actions/storefront/checkout/CheckoutAction.js
+++ tests/Cypress/cypress/support/actions/storefront/checkout/CheckoutAction.js
@@ -124,8 +124,8 @@ export default class CheckoutAction {
         if (shopware.isVersionGreaterEqual('6.7.0.0')) {
             cy.get('.confirm-address .card-actions:eq(0) > a').click();
             cy.wait(1000);
-            cy.get('.modal-dialog-address #shipping-address-tab-pane .address-manager-select-address button').click();
-            cy.get('.dropdown-menu .address-manager-modal-address-form[data-address-type="shipping"]').click();
+            cy.get('.modal-dialog-address #shipping-address-tab-pane .address-manager-select-address button').first().click();
+            cy.get('.dropdown-menu .address-manager-modal-address-form[data-address-type="shipping"]').first().click();
         }
 
         if (shopware.isVersionLower('6.7.0.0')) {
