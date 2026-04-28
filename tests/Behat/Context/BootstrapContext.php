a2d26675f499e46c407483ed9372e2a7c366c148
Vitalij Mik <vitalij.mik@dasistweb.de>
2026-04-28 10:00:05 +0200

    NTR: update behat tests

--- tests/Behat/Context/BootstrapContext.php
+++ tests/Behat/Context/BootstrapContext.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Mollie\Shopware\Behat\Context;
 
 use Behat\Behat\Context\Context;
+use Behat\Hook\BeforeScenario;
 use Behat\Hook\BeforeSuite;
 use Mollie\Shopware\Behat\Storage;
 use Mollie\Shopware\Component\Settings\SettingsService;
@@ -13,17 +14,13 @@ final class BootstrapContext implements Context
 {
     use IntegrationTestBehaviour;
 
-    /**
-     * @BeforeSuite
-     */
+    #[BeforeSuite]
     public static function bootstrap(): void
     {
-        require_once __DIR__ . '/../../bootstrap.php'; // or just inline your bootstrapping here, depending what you need
+        require_once __DIR__ . '/../../bootstrap.php';
     }
 
-    /**
-     * @BeforeScenario
-     */
+    #[BeforeScenario]
     public function clearStorage(): void
     {
         /** @var SettingsService $settingsService */
