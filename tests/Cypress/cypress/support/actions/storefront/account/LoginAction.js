3a0486bbc66b5ab824b9a4df1826f341c6caf371
Vitalij Mik <vitalij.mik@dasistweb.de>
2026-05-13 14:46:03 +0200

    NTR: test normal login without session

--- tests/Cypress/cypress/support/actions/storefront/account/LoginAction.js
+++ tests/Cypress/cypress/support/actions/storefront/account/LoginAction.js
@@ -7,28 +7,18 @@ export default class LoginAction {
      */
     doLogin(email, password) {
 
-        cy.session('login', () => {
-
-            cy.request({
-                method: 'POST',
-                url: '/account/login',
-                form: true,
-                followRedirect: false,
-                failOnStatusCode: false,
-                body: {
-                    username: email,
-                    password: password,
-                    redirectTo: 'frontend.account.home.page',
-                    redirectParameters: '[]',
-                },
-            });
-
-        }, {
-            cacheAcrossSpecs: false,
-            validate() {
-                cy.visit('/account');
-                cy.url().should('not.include', '/login');
-            }
+        cy.request({
+            method: 'POST',
+            url: '/account/login',
+            form: true,
+            followRedirect: false,
+            failOnStatusCode: false,
+            body: {
+                username: email,
+                password: password,
+                redirectTo: 'frontend.account.home.page',
+                redirectParameters: '[]',
+            },
         });
 
         cy.visit('/');
