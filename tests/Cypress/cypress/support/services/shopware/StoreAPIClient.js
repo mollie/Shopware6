5c270eb19c7d386ae62c6e2bec443451892e540a
Vitalij Mik <vitalij.mik@dasistweb.de>
2026-06-12 11:45:56 +0200

    NTR: refactor storeapi from axios to fetch api

--- tests/Cypress/cypress/support/services/shopware/StoreAPIClient.js
+++ tests/Cypress/cypress/support/services/shopware/StoreAPIClient.js
@@ -1,221 +1,73 @@
-import axios from 'axios';
-
 export default class StoreAPIClient {
 
-
-    /**
-     *
-     * @param salesChannelApiKey
-     */
     constructor(salesChannelApiKey) {
         this.salesChannelApiKey = salesChannelApiKey;
-        this.authInformation = {};
-        this.basePath = '';
-
-        this.clearContextToken();
-
-        this.client = axios.create({
-            baseURL: `${Cypress.config('baseUrl')}/store-api`,
-            timeout: 10000,
-        });
+        this.contextToken = null;
+        this.baseURL = `${Cypress.config('baseUrl')}/store-api`;
     }
 
-    /**
-     *
-     * @param token
-     */
     setContextToken(token) {
         this.contextToken = token;
     }
 
-    /**
-     *
-     */
     clearContextToken() {
         this.contextToken = null;
     }
 
-    /**
-     * @param email
-     * @param password
-     * @returns {Promise}
-     */
-    login(email, password) {
-        return this.post('/account/login', {
-            email: email,
-            password: password,
-        }).then((response) => {
-            let token;
+    async login(email, password) {
+        const response = await this.post('/account/login', { email, password });
+        const token = response.data?.contextToken ?? response.headers.get('sw-context-token');
 
-            if (response && response.data && response.data.contextToken) {
-                token = response.data.contextToken;
-            } else if (response && response.headers && response.headers['sw-context-token']) {
-                token = response.headers['sw-context-token'];
-            }
-
-            if (token) {
-                this.setContextToken(token);
-            }
-
-            return response;
-        });
+        if (token) {
+            this.setContextToken(token);
+        }
+        return response;
     }
 
-
-    /**
-     *
-     * @param url
-     * @param data
-     * @param params
-     * @returns {*}
-     */
-    get(url, params = {}) {
-        return this.request({
-            method: 'get',
-            url,
-            params
-        });
+    async get(url, params = {}) {
+        return this.request('GET', url, null, params);
     }
 
-    /**
-     *
-     * @param url
-     * @param data
-     * @param params
-     * @returns {*}
-     */
-    post(url, data, params = {}) {
-        return this.request({
-            method: 'post',
-            url,
-            data,
-            params
-        });
+    async post(url, data = null, params = {}) {
+        return this.request('POST', url, data, params);
     }
 
-    /**
-     *
-     * @param url
-     * @param data
-     * @param params
-     * @returns {*}
-     */
-    delete(url, params = {}) {
-        return this.request({
-            method: 'delete',
-            url,
-            params
-        });
+    async put(url, data = null, params = {}) {
+        return this.request('PUT', url, data, params);
     }
 
-    /**
-     *
-     * @param url
-     * @param data
-     * @param params
-     * @returns {*}
-     */
-    head(url, params = {}) {
-        return this.request({
-            method: 'head',
-            url,
-            params
-        });
+    async patch(url, data = null, params = {}) {
+        return this.request('PATCH', url, data, params);
     }
 
-    /**
-     *
-     * @param url
-     * @param data
-     * @param params
-     * @returns {*}
-     */
-    options(url, params = {}) {
-        return this.request({
-            method: 'options',
-            url,
-            params
-        });
+    async delete(url, params = {}) {
+        return this.request('DELETE', url, null, params);
     }
 
-    /**
-     *
-     * @param url
-     * @param data
-     * @param params
-     * @returns {*}
-     */
-    put(url, data, params = {}) {
-        return this.request({
-            method: 'put',
-            data,
-            url,
-            params
-        });
-    }
+    async request(method, url, data = null, params = {}) {
+        const queryString = Object.keys(params).length
+            ? '?' + new URLSearchParams(params).toString()
+            : '';
 
-    /**
-     *
-     * @param url
-     * @param data
-     * @param params
-     * @returns {*}
-     */
-    patch(url, data, params = {}) {
-        return this.request({
-            method: 'patch',
-            data,
-            url,
-            params
-        });
-    }
-
-    /**
-     *
-     * @param url
-     * @param method
-     * @param params
-     * @param data
-     * @returns {*}
-     */
-    request({url, method, params, data}) {
-
-        const requestConfig = {
-            headers: this.getHeaders(),
-            url,
-            method,
-            params,
-            data
-        };
-
-        return this.client.request(requestConfig)
-            .then((response) => {
-                return response;
-            })
-            .catch(function (error) {
-                return {
-                    'data': error.response,
-                }
-            });
-    }
-
-    /**
-     * Returns the necessary headers for the administration API requests
-     *
-     * @returns {Object}
-     */
-    getHeaders() {
         const headers = {
             Accept: 'application/vnd.api+json',
             'sw-access-key': this.salesChannelApiKey,
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         };
 
         if (this.contextToken) {
             headers['sw-context-token'] = this.contextToken;
         }
 
-        return headers;
+        const options = { method, headers };
+        if (data !== null) {
+            options.body = JSON.stringify(data);
+        }
+
+        const res = await fetch(`${this.baseURL}${url}${queryString}`, options);
+        const body = await res.json().catch(() => null);
+
+        return { status: res.status, data: body, headers: res.headers };
     }
 
 }
