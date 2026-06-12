93db699a3c5926e2b5ecc107a1c250c34d60661f
Vitalij Mik <vitalij.mik@dasistweb.de>
2026-06-12 12:23:15 +0200

    NTR: refactor store api tests

--- tests/Cypress/cypress/e2e/store-api/applepay-direct.cy.js
+++ tests/Cypress/cypress/e2e/store-api/applepay-direct.cy.js
@@ -18,176 +18,106 @@ describe('Apple Pay Direct - Store API Routes', () => {
          * without Mollie API keys, this might not return a valid ID.
          * Instead we just verify that the route is available and that the response is existing.
          */
-        it('C266669: Route available /store-api/mollie/applepay/id @core', () => {
+        it('C266669: Route available /store-api/mollie/applepay/id @core', async () => {
+            const response = await storeApiClient.get('/mollie/applepay/id');
 
-            const request = new Promise((resolve) => {
-                storeApiClient.get('/mollie/applepay/id').then(response => {
-                    resolve({'data': response.data});
-                });
-            })
-
-            cy.wrap(request).its('data').then(data => {
-                cy.wrap(data).its('apiAlias').should('eq', 'mollie_payments_applepay_direct_id')
-                cy.wrap(data).its('success').should('exist', true)
-                cy.wrap(data).its('id').should('exist', true)
-            });
-        })
+            expect(response.data.apiAlias).to.eq('mollie_payments_applepay_direct_id');
+            expect(response.data.success).to.exist;
+            expect(response.data.id).to.exist;
+        });
 
     })
 
     context(storeApiPrefix + "/mollie/applepay/enabled", () => {
 
-        it('C266670: Route available /store-api/mollie/applepay/enabled @core', () => {
+        it('C266670: Route available /store-api/mollie/applepay/enabled @core', async () => {
+            const response = await storeApiClient.get('/mollie/applepay/enabled');
 
-            const request = new Promise((resolve) => {
-                storeApiClient.get('/mollie/applepay/enabled').then(response => {
-                    resolve({'data': response.data});
-                });
-            })
-
-            cy.wrap(request).its('data').then(data => {
-                cy.wrap(data).its('apiAlias').should('eq', 'mollie_payments_applepay_direct_enabled')
-                cy.wrap(data).its('enabled').should('exist', true)
-            });
-        })
+            expect(response.data.apiAlias).to.eq('mollie_payments_applepay_direct_enabled');
+            expect(response.data.enabled).to.exist;
+        });
 
     })
 
     context(storeApiPrefix + "/mollie/applepay/cart", () => {
 
-        it('C266673: Apple Pay Direct get cart structure (Store API) @core', () => {
+        it('C266673: Apple Pay Direct get cart structure (Store API) @core', async () => {
+            const response = await storeApiClient.get('/mollie/applepay/cart');
 
-            const request = new Promise((resolve) => {
-                storeApiClient.get('/mollie/applepay/cart').then(response => {
-                    resolve({'data': response.data});
-                });
-            })
-
-            cy.wrap(request).its('data').then(response => {
-                cy.wrap(response).its('apiAlias').should('eq', 'mollie_payments_applepay_direct_cart')
-                cy.wrap(response).its('cart.label').its('length').should('be.gt', 0)
-                cy.wrap(response).its('cart.total.amount').should('eq', 0)
-            });
-        })
+            expect(response.data.apiAlias).to.eq('mollie_payments_applepay_direct_cart');
+            expect(response.data.cart.label.length).to.be.gt(0);
+            expect(response.data.cart.total.amount).to.eq(0);
+        });
 
     })
 
     context(storeApiPrefix + "/mollie/applepay/validate", () => {
 
-        it('C266674: Apple pay Direct validate merchant domain (Store API) @core', () => {
+        it('C266674: Apple pay Direct validate merchant domain (Store API) @core', async () => {
+            const response = await storeApiClient.post('/mollie/applepay/validate');
 
-            const request = new Promise((resolve) => {
-                storeApiClient.post('/mollie/applepay/validate').then(response => {
-                    resolve({'data': response.data});
-                });
-            })
-
-            cy.wrap(request).its('data').then(response => {
-                cy.wrap(response).its('status').should('eq', 500)
-                expect(response.data.errors[0].detail).to.contain('Please provide a validation url');
-            });
-        })
+            expect(response.status).to.eq(500);
+            expect(response.data.errors[0].detail).to.contain('Please provide a validation url');
+        });
 
     })
 
     context(storeApiPrefix + "/mollie/applepay/shipping-methods", () => {
 
-        it('C266675: Apple pay Direct Shipping Methods without country code (Store API) @core', () => {
+        it('C266675: Apple pay Direct Shipping Methods without country code (Store API) @core', async () => {
+            const response = await storeApiClient.post('/mollie/applepay/shipping-methods');
 
-            const request = new Promise((resolve) => {
-                storeApiClient.post('/mollie/applepay/shipping-methods').then(response => {
-                    resolve({'data': response.data});
-                });
-            })
+            expect(response.status).to.eq(500);
+            expect(response.data.errors[0].detail).to.contain('No Country Code provided');
+        });
 
-            cy.wrap(request).its('data').then(response => {
-                cy.wrap(response).its('status').should('eq', 500)
-                expect(response.data.errors[0].detail).to.contain('No Country Code provided');
-            });
-        })
+        it('C266676: Apple pay Direct Shipping Methods with country code (Store API) @core', async () => {
+            const response = await storeApiClient.post('/mollie/applepay/shipping-methods', { countryCode: 'DE' });
 
-        it('C266676: Apple pay Direct Shipping Methods with country code (Store API) @core', () => {
-
-            const request = new Promise((resolve) => {
-                storeApiClient.post('/mollie/applepay/shipping-methods', {'countryCode': 'DE'}).then(response => {
-                    resolve({'data': response.data});
-                });
-            })
-
-            cy.wrap(request).its('data').then(response => {
-                cy.wrap(response).its('apiAlias').should('eq', 'mollie_payments_applepay_direct_shipping_methods')
-                cy.wrap(response).its('shippingMethods').should('exist', true);
-            });
-        })
+            expect(response.data.apiAlias).to.eq('mollie_payments_applepay_direct_shipping_methods');
+            expect(response.data.shippingMethods).to.exist;
+        });
 
     })
 
 
     context(storeApiPrefix + "/mollie/applepay/shipping-method", () => {
 
-        it('C266677: Apple pay Direct set shipping without identifier (Store API) @core', () => {
+        it('C266677: Apple pay Direct set shipping without identifier (Store API) @core', async () => {
+            const response = await storeApiClient.post('/mollie/applepay/shipping-method');
 
-            const request = new Promise((resolve) => {
-                storeApiClient.post('/mollie/applepay/shipping-method').then(response => {
-                    resolve({'data': response.data});
-                });
-            })
+            expect(response.status).to.eq(500);
+            expect(response.data.errors[0].detail).to.contain('Missing shipping method identifier');
+        });
 
-            cy.wrap(request).its('data').then(response => {
-                cy.wrap(response).its('status').should('eq', 500)
-                expect(response.data.errors[0].detail).to.contain('Missing shipping method identifier');
-            });
-        })
+        it('C266678: Apple pay Direct set shipping with invalid identifier (Store API) @core', async () => {
+            const response = await storeApiClient.post('/mollie/applepay/shipping-method', { identifier: 'abc' });
 
-        it('C266678: Apple pay Direct set shipping with invalid identifier (Store API) @core', () => {
-
-            const request = new Promise((resolve) => {
-                storeApiClient.post('/mollie/applepay/shipping-method', {'identifier': 'abc'}).then(response => {
-                    resolve({'data': response.data});
-                });
-            })
-
-            cy.wrap(request).its('data').then(response => {
-                cy.wrap(response).its('status').should('eq', 400)
-                expect(response.data.errors[0].detail).to.contain('Value is not a valid UUID: abc');
-            });
-        })
+            expect(response.status).to.eq(400);
+            expect(response.data.errors[0].detail).to.contain('Value is not a valid UUID: abc');
+        });
 
     })
 
 
     context(storeApiPrefix + "/mollie/applepay/pay", () => {
 
-        it('C266680: Apple pay Direct pay with invalid payment token (Store API) @core', () => {
+        it('C266680: Apple pay Direct pay with invalid payment token (Store API) @core', async () => {
+            const response = await storeApiClient.post('/mollie/applepay/pay');
 
-            const request = new Promise((resolve) => {
-                storeApiClient.post('/mollie/applepay/pay').then(response => {
-                    resolve({'data': response.data});
-                });
-            })
-
-            cy.wrap(request).its('data').then(response => {
-                cy.wrap(response).its('status').should('eq', 500)
-                expect(response.data.errors[0].detail).to.contain('"paymentToken" not set in request body')
-            });
-        })
+            expect(response.status).to.eq(500);
+            expect(response.data.errors[0].detail).to.contain('"paymentToken" not set in request body');
+        });
 
     })
 
     context(storeApiPrefix + "/mollie/applepay/restore-cart", () => {
 
-        it('C266681: Apple Pay Direct restore cart (Store API) @core', () => {
+        it('C266681: Apple Pay Direct restore cart (Store API) @core', async () => {
+            const response = await storeApiClient.post('/mollie/applepay/restore-cart');
 
-            const request = new Promise((resolve) => {
-                storeApiClient.post('/mollie/applepay/restore-cart').then(response => {
-                    resolve({'data': response.data});
-                });
-            })
-
-            cy.wrap(request).its('data').then(response => {
-                cy.wrap(response).its('apiAlias').should('eq', 'mollie_payments_applepay_direct_cart_restored')
-            });
-        })
+            expect(response.data.apiAlias).to.eq('mollie_payments_applepay_direct_cart_restored');
+        });
 
     })
 
