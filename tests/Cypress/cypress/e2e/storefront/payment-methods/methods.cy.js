52a75c910542c56973f8b8ee3f58ad7f8bb539fb
Christian <boxblinkracer@gmx.net>
2026-06-17 14:41:07 +0200

    NTR: fix cypress test

--- tests/Cypress/cypress/e2e/storefront/payment-methods/methods.cy.js
+++ tests/Cypress/cypress/e2e/storefront/payment-methods/methods.cy.js
@@ -31,25 +31,30 @@ context('Active Payment Methods', () => {
 
             it('C3996: Mollie Payment Methods show TEST MODE @core', () => {
 
-                devices.setDevice(device);
+                cy.wrap(null).then(() => {
+                    const shopConfig = new ShopConfiguration();
+                    const pluginConfig = new PluginConfiguration();
 
-                const shopConfig = new ShopConfiguration();
-                const pluginConfig = new PluginConfiguration();
+                    pluginConfig.setMollieFailureMode(true);
+                    configAction.configureEnvironment(shopConfig, pluginConfig);
 
-                pluginConfig.setMollieFailureMode(true);
-
-                configAction.configureEnvironment(shopConfig, pluginConfig);
-
-                session.resetBrowserSession();
+                    session.resetBrowserSession();
+                    devices.setDevice(device);
+                });
 
                 scenarioDummyBasket.execute();
+
+                // make sure the confirm page is fully rendered before we
+                // probe for the Test-mode label - in CI the confirm template
+                // can take longer than the default command timeout
+                cy.get('#confirmFormSubmit', { timeout: 30000 }).should('be.visible');
 
                 paymentAction.showPaymentMethods();
 
                 // yes we require test mode, but this is
                 // the only chance to see if the plugin is being used, because
                 // every merchant might have different payment methods ;)
-                cy.contains('(Test mode)');
+                cy.contains('(Test mode)', { timeout: 30000 }).should('be.visible');
             })
         })
     })
