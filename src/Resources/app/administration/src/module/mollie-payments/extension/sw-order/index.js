17a3ad5d8fdf30032d87155fe600524bdbcf4479
Vitalij Mik <vitalij.mik@dasistweb.de>
2026-06-19 13:34:42 +0200

    NTR: remove sw-order-user-card

--- src/Resources/app/administration/src/module/mollie-payments/extension/sw-order/index.js
+++ src/Resources/app/administration/src/module/mollie-payments/extension/sw-order/index.js
@@ -1,5 +1,4 @@
 import './component/sw-order-line-items-grid';
-import './component/sw-order-user-card';
 import './view/mollie-order-tab';
 import './page/sw-order-detail';
 import './page/sw-order-list';
