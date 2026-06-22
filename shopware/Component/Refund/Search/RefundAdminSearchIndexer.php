36a0397fd572ea137220fa6bd28efde6b164b06d
Vitalij Mik <vitalij.mik@dasistweb.de>
2026-06-22 11:36:41 +0200

    NTR: move refund ES indexer

--- shopware/Component/Refund/Search/RefundAdminSearchIndexer.php
+++ shopware/Component/Refund/Search/RefundAdminSearchIndexer.php
@@ -1,46 +1,39 @@
 <?php
 declare(strict_types=1);
 
-namespace Kiener\MolliePayments\Components\RefundManager\Elasticsearch;
+namespace Mollie\Shopware\Component\Refund\Search;
 
 use Doctrine\DBAL\ArrayParameterType;
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\ParameterType;
-use Kiener\MolliePayments\Components\RefundManager\DAL\Refund\RefundCollection;
-use Kiener\MolliePayments\Components\RefundManager\DAL\Refund\RefundDefinition;
-use Kiener\MolliePayments\Components\RefundManager\DAL\Refund\RefundEntity;
+use Mollie\Shopware\Component\Refund\DAL\Refund\RefundCollection;
+use Mollie\Shopware\Component\Refund\DAL\Refund\RefundDefinition;
+use Mollie\Shopware\Component\Refund\DAL\Refund\RefundEntity;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Dbal\Common\IterableQuery;
 use Shopware\Core\Framework\DataAbstractionLayer\Dbal\Common\IteratorFactory;
-use Shopware\Core\Framework\DataAbstractionLayer\Entity;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
 use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Elasticsearch\Admin\Indexer\AbstractAdminIndexer;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
-class RefundAdminSearchIndexer extends AbstractAdminIndexer
+#[AutoconfigureTag('shopware.elastic.admin-searcher-index', ['key' => 'mollie_refund'])]
+final class RefundAdminSearchIndexer extends AbstractAdminIndexer
 {
-    private Connection $connection;
-    private IteratorFactory $factory;
-    /** @var EntityRepository<RefundCollection<RefundEntity>> */
-    private $repository;
-    private int $indexingBatchSize;
-
     /**
      * @param EntityRepository<RefundCollection<RefundEntity>> $repository
      */
     public function __construct(
-        Connection $connection,
-        IteratorFactory $factory,
-        $repository,
-        int $indexingBatchSize
+        private readonly Connection $connection,
+        private readonly IteratorFactory $factory,
+        #[Autowire(service: 'mollie_refund.repository')]
+        private readonly EntityRepository $repository,
+        #[Autowire(value: '%elasticsearch.indexing_batch_size%')]
+        private readonly int $indexingBatchSize
     ) {
-        $this->connection = $connection;
-        $this->factory = $factory;
-        $this->repository = $repository;
-        $this->indexingBatchSize = $indexingBatchSize;
     }
 
     public function getDecorated(): AbstractAdminIndexer
@@ -70,35 +63,23 @@ class RefundAdminSearchIndexer extends AbstractAdminIndexer
      */
     public function fetch(array $ids): array
     {
-        if (class_exists(ArrayParameterType::class)) {
-            $type = ArrayParameterType::BINARY;
-        } else {
-            $type = ParameterType::BINARY + Connection::ARRAY_PARAM_OFFSET;
-        }
-
         $data = $this->connection->fetchAllAssociative(
-            '
-            SELECT LOWER(HEX(mollie_refund.id)) as id,
-                   type,
-                   public_description,
-                   internal_description
-            FROM mollie_refund
-            WHERE mollie_refund.id IN (:ids)
-            GROUP BY mollie_refund.id
-        ',
-            [
-                'ids' => Uuid::fromHexToBytesList($ids),
-            ],
-            [
-                'ids' => $type
-            ]
+            'SELECT LOWER(HEX(mollie_refund.id)) as id,
+                    type,
+                    public_description,
+                    internal_description
+             FROM mollie_refund
+             WHERE mollie_refund.id IN (:ids)
+             GROUP BY mollie_refund.id',
+            ['ids' => Uuid::fromHexToBytesList($ids)],
+            ['ids' => ArrayParameterType::BINARY]
         );
 
         $mapped = [];
         foreach ($data as $row) {
             $id = (string) $row['id'];
-            $text = \implode(' ', array_filter($row));
-            $mapped[$id] = ['id' => $id, 'text' => \strtolower($text)];
+            $text = implode(' ', array_filter($row));
+            $mapped[$id] = ['id' => $id, 'text' => strtolower($text)];
         }
 
         return $mapped;
@@ -108,8 +89,6 @@ class RefundAdminSearchIndexer extends AbstractAdminIndexer
      * @param array<string, mixed> $result
      *
      * @return array{total:int, data:EntityCollection<RefundEntity>}
-     *
-     * Return EntityCollection<Entity> and their total by ids in the result parameter
      */
     public function globalData(array $result, Context $context): array
     {
