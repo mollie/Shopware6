318f075fe2eff85136d336147d1770e61870f5f7
Vitalij Mik <vitalij.mik@dasistweb.de>
2026-08-26 15:55:44 +0200

    NTR: try to fix behat and vitest

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
     test: {
         include: ['./src/Resources/app/**/*.spec.js'],
         watch: false,
+        // Writes Allure results for the CI report, next to the PHP suites and Cypress.
+        setupFiles: ['allure-vitest/setup'],
+        reporters: [
+            'default',
+            ['allure-vitest/reporter', {resultsDir: './.reports/allure/results'}],
+        ],
     },
 })
